5530275c81b64ef1c3e73c07ccf72f86f1c007f2
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-05-05 08:34:15 +0300

    Time: 0 ms (100.00%), Space: 41.1 MB (16.49%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] r = s.split(" ");
-        return r[r.length - 1].length();
+        int i = s.length() - 1;
+        while(s.charAt(i) == ' '){
+            i--;
+        }
+        int length = 0;
+        while(i >= 0 && s.charAt(i) != ' '){
+            length++;
+            i--;
+        }
+        return length;
     }
 }
