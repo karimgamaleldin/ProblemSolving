cfa235c4f0754991457a4f09784f055067def668
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-05-05 11:28:22 +0300

    Time: 23 ms (17.09%), Space: 42.5 MB (62.46%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -4,16 +4,18 @@ class Solution {
         HashMap<Character , Character > hash2 = new HashMap<Character , Character >();
         
         for(int i = 0 ; i < s.length() ; i++){
-            if(hash1.containsKey(s.charAt(i))){
-                char x  = hash1.get(s.charAt(i));
-                if ( x != t.charAt(i) ) return false;
-            }
-            if(hash2.containsKey(t.charAt(i))){
-                char x  = hash2.get(t.charAt(i));
-                if ( x != s.charAt(i) ) return false;
-            }
-            hash1.put(s.charAt(i) , t.charAt(i));
-            hash2.put(t.charAt(i) , s.charAt(i));
+            // if(hash1.containsKey(s.charAt(i))){ to be faster
+            //     char x  = hash1.get(s.charAt(i));
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+            if ( hash1.getOrDefault(sc , tc) != tc)  return false;
+            // }
+            // if(hash2.containsKey(t.charAt(i))){
+            //    char x  = hash2.get(t.charAt(i));
+            if ( hash2.getOrDefault(tc , sc) != sc)  return false;
+            // }
+            hash1.put(sc , tc);
+            hash2.put(tc , sc);
         }
         return true;
     }
