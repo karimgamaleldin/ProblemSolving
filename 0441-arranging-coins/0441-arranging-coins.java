a2f8f115ed911241fba4cf55944b759fcaa2400d
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-05-07 14:42:05 +0300

    Time: 2 ms (78.89%), Space: 40.7 MB (5.33%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 1;
+        long end = n;
+        long r = 0;
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            long x = mid * (mid+1) / 2;
+            if(x <= n){
+                r = Math.max(mid , r);
+                start = mid + 1;
+            }
+            else end = mid - 1;
+        }
+        return (int)r;
+    }
+    
+}
