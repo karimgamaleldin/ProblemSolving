09474d20548c4b9513d7f8fb947c46a0840e0905
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-06-28 17:43:53 +0300

    Time: 80 ms (57.42%), Space: 44.6 MB (51.98%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public String removeDuplicates(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i = 0 ; i < s.length() ; i++){
+        for(int i = s.length() - 1 ; i >= 0 ; i--){
             if (stack.isEmpty()) stack.push(s.charAt(i));
             else if(stack.peek() != s.charAt(i)){
                 stack.push(s.charAt(i));
@@ -11,6 +11,6 @@ class Solution {
         while(!stack.isEmpty()){
             sb.append(stack.pop());
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
