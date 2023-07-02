fa3b7c99109b39dee043562cd98a21d90e423764
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-02 15:47:52 +0300

    Time: 154 ms (63.13%), Space: 55.7 MB (59.43%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int i = 0;
+
+        while (i < heights.length) {
+            if (stack.isEmpty() || heights[i] >= heights[stack.peek()]) {
+                stack.push(i);
+                i++;
+            } else {
+                int top = stack.pop();
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                int area = heights[top] * width;
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            int width = stack.isEmpty() ? heights.length : heights.length - stack.peek() - 1;
+            int area = heights[top] * width;
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+}
