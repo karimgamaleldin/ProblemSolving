efbd7b9d95cc9cd04a0517a88507a352fc09f403
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-02 10:28:59 +0300

    Time: 56 ms (7.46%), Space: 43.2 MB (7.08%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+        long min = 0;
+        long max = x/2 + 1;
+        while(min <= max){
+            long mid = (max + min) / 2;
+            System.out.println(mid);
+            if(mid * mid == x || ((mid + 1) * (mid + 1) > x && mid * mid < x)) return (int) mid;
+            else if((mid - 1) * (mid - 1) < x && mid * mid > x) return (int) (mid - 1);
+            else if( mid*mid > x ) max = mid - 1;
+            else min = mid + 1;
+        }
+        return 0;
+    }
+}
