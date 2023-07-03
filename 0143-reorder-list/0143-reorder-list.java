f273b95c5507559affda9c2bcbc7df87fd08c139
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-03 12:40:06 +0300

    Time: 1 ms (100.00%), Space: 46.3 MB (32.08%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
+        // Reverse solution:
         if(head.next == null) return;
         ListNode fast = head.next;
         ListNode slow = head;
@@ -17,18 +18,45 @@ class Solution {
             fast = fast.next.next;
             slow = slow.next;
         }
-        Stack<ListNode> s = new Stack<>();
-        while(slow != null){
-            s.push(slow);
-            slow = slow.next;
+        ListNode prev = null;
+        ListNode curr = slow.next;
+        slow.next = null;
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
         }
         slow = head;
-        while(!s.isEmpty()){
-            ListNode temp = s.pop();
-            temp.next = slow.next;
-            slow.next = temp;
-            slow = slow.next.next;
+        while(prev != null){
+            ListNode a = slow.next;
+            ListNode b = prev.next;
+            slow.next = prev;
+            prev.next = a;
+            slow = a;
+            prev = b;
         }
-        slow.next = null;
+        
+        //Stack solution:
+        // if(head.next == null) return;
+        // ListNode fast = head.next;
+        // ListNode slow = head;
+        // while(fast!= null && fast.next != null){
+        //     fast = fast.next.next;
+        //     slow = slow.next;
+        // }
+        // Stack<ListNode> s = new Stack<>();
+        // while(slow != null){
+        //     s.push(slow);
+        //     slow = slow.next;
+        // }
+        // slow = head;
+        // while(!s.isEmpty()){
+        //     ListNode temp = s.pop();
+        //     temp.next = slow.next;
+        //     slow.next = temp;
+        //     slow = slow.next.next;
+        // }
+        // slow.next = null;
     }
 }
