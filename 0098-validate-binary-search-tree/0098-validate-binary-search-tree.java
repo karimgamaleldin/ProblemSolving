c78df87780d4a92ad80fbb002904106bf7f3c705
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-09 01:42:56 +0300

    Time: 0 ms (100.00%), Space: 44.4 MB (12.71%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,18 +15,26 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        List<Integer> l = new ArrayList<>();
-        helper(root , l);
-        for(int i = 0 ; i < l.size() - 1 ; i++){
-            if(l.get(i) >= l.get(i+1)) return false;
-        }
-        return true;
+        return helper(root.left , Long.MIN_VALUE , root.val) && helper(root.right , root.val , Long.MAX_VALUE);
+    }
+    public boolean helper(TreeNode root , long min , long max){
+        if(root == null) return true;
+        if(root.val >= max || root.val <= min) return false;
+        return helper(root.left , min , root.val) && helper(root.right , root.val , max);
+    }
+//     public boolean isValidBST(TreeNode root) {
+//         List<Integer> l = new ArrayList<>();
+//         helper(root , l);
+//         for(int i = 0 ; i < l.size() - 1 ; i++){
+//             if(l.get(i) >= l.get(i+1)) return false;
+//         }
+//         return true;
         
-    }
-    public void helper(TreeNode root , List<Integer> l){
-        if(root == null) return;
-        helper(root.left , l);
-        l.add(root.val);
-        helper(root.right , l);
-    }
+//     }
+//     public void helper(TreeNode root , List<Integer> l){
+//         if(root == null) return;
+//         helper(root.left , l);
+//         l.add(root.val);
+//         helper(root.right , l);
+//     }
 }
