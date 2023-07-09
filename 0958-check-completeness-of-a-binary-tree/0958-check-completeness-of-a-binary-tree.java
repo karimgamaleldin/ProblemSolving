595c685d47138351d3d9ca7383d4cf28e8cdf234
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-09 17:26:34 +0300

    Time: 3 ms (8.04%), Space: 41.1 MB (70.58%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        int level = 0;
+        Queue<Pair<TreeNode , Integer>> q = new LinkedList<>();
+        q.add(new Pair(root , 0));
+        while(!q.isEmpty()){
+            int size = q.size();
+            Pair<TreeNode , Integer> firstNode = q.peek();
+            Pair<TreeNode , Integer> lastNode = firstNode;
+            for(int i = 0 ; i < size ; i++){
+                Pair<TreeNode , Integer> currNode = q.remove();
+                lastNode = currNode;
+                if(currNode.getKey().left != null) q.add(new Pair(currNode.getKey().left , 2*currNode.getValue() + 1));
+                if(currNode.getKey().right != null) q.add(new Pair(currNode.getKey().right , 2*currNode.getValue() + 2));
+                if(!q.isEmpty() && currNode.getValue() + 1 != q.peek().getValue()) return false;
+            }
+            if(!q.isEmpty() && size != Math.pow(2,level)) return false;
+            level++;
+        }
+        return true;
+    }
+}
