511247b20ad43908591ffa3c85032970f8846ba9
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-16 06:08:17 +0300

    Time: 1 ms (58.77%), Space: 43.5 MB (19.36%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,6 +2,13 @@ class Solution {
     Map<Integer , Integer> memo = new HashMap<>();
     int[] arr;
     public int minCostClimbingStairs(int[] cost) {
+        int[] tab = new int[cost.length];
+        for(int i = 2 ; i < cost.length ; i++){
+            tab[i] = Math.min(cost[i - 1] + tab[i - 1] , cost[i - 2] + tab[i - 2]);
+        }
+        return Math.min(cost[cost.length - 1] + tab[cost.length - 1] , cost[cost.length - 2] + tab[cost.length - 2]);
+    }
+    public int minCostClimbingStairsBottomUp(int[] cost) {
         arr = cost;
         dp(cost.length);
         return memo.get(cost.length);
