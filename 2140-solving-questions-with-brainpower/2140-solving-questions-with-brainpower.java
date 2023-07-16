c5a9a0c8e826546c4771bcbcff212a956e443f1e
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-16 08:18:02 +0300

    Time: 5 ms (98.13%), Space: 97.9 MB (60.19%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,15 +1,25 @@
 class Solution {
-    Map<Integer , Long> memo = new HashMap<>();
     public long mostPoints(int[][] questions) {
-        return dp(0 , questions);
-    }
-    public long dp(int i , int[][] arr){
-        if(i >= arr.length) return 0;
-        if(memo.containsKey(i)){
-            return memo.get(i);
+        long tab[] = new long[questions.length];
+        tab[questions.length - 1] = questions[questions.length - 1][0];
+        for(int i = questions.length - 2 ; i >= 0 ; i--){
+            int j = i + questions[i][1] + 1;
+            long x = j < questions.length ? tab[j] : 0L;
+            tab[i] = Math.max(questions[i][0] + x , tab[i + 1]);
         }
-        int j = i + arr[i][1] + 1;
-        memo.put(i, Math.max(arr[i][0] + dp(j, arr), dp(i + 1, arr)));
-        return memo.get(i);
+        return tab[0];
     }
+    // Map<Integer , Long> memo = new HashMap<>();
+    // public long mostPoints(int[][] questions) {
+    //     return dp(0 , questions);
+    // }
+    // public long dp(int i , int[][] arr){
+    //     if(i >= arr.length) return 0;
+    //     if(memo.containsKey(i)){
+    //         return memo.get(i);
+    //     }
+    //     int j = i + arr[i][1] + 1;
+    //     memo.put(i, Math.max(arr[i][0] + dp(j, arr), dp(i + 1, arr)));
+    //     return memo.get(i);
+    // }
 }
