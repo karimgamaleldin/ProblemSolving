76255d2acb3297cd5c8cc20dcd6985bc6939b3f4
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-18 01:28:41 +0300

    Time: 23 ms (21.86%), Space: 48.7 MB (5.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        HashMap<String , ArrayList<String>> map = new HashMap<String , ArrayList<String>>();
+        Map<String , List<String>> map = new HashMap<>();
         for(int i = 0 ; i < strs.length ; i++){
-            int[] characters = new int[26]; //sorted array of characters to know the amount of each characters (initialized by 0)
-            for(int j = 0 ; j < strs[i].length() ; j++){
-                char c = strs[i].charAt(j);
-                ++characters[c - 'a']; // to increment the number of c in the array
+            String s = strs[i];
+            int n = s.length();
+            int[] alph = new int[26];
+            for(int j = 0 ; j < n ; j++){
+                alph[s.charAt(j) - 'a']++;
             }
-            String x = new String(Arrays.toString(characters)); //String of the shape of an array like: [1,2,4]
-            if(map.containsKey(x)) map.get(x).add(strs[i]); //check if the sorted like array of present get the list and add
-            else {
-                ArrayList<String> y = new ArrayList<String>();
-                y.add(strs[i]);
-                map.put(x , y);
-            }
+            String key = Arrays.toString(alph);
+            if(!map.containsKey(key)) map.put(key , new ArrayList<>());
+            map.get(key).add(s);
         }
-        result.addAll(map.values()); 
-        return result;
+        List<List<String>> r = new ArrayList<>();
+        for(List<String> value : map.values()){
+            r.add(value);
+        }
+        return r;
     }
 }
