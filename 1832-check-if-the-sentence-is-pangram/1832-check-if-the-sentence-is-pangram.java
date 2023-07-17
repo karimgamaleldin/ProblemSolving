4a5db7c3379e0c21b3f8c408e21c9d2247da3658
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-17 19:49:34 +0300

    Time: 1 ms (86.16%), Space: 40.6 MB (71.16%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
         int n = sentence.length();
-        HashSet<Character> set = new HashSet<>();
-        for(int i = 0 ; i < n ; i++) {
-            set.add(sentence.charAt(i));
-            if(set.size() == 26) return true;
+        int all = 1 << 26;
+        int found = 0;
+        for(int i = 0 ; i < n ;i++){
+            int t = sentence.charAt(i) - 'a';
+            t = 1 << t;
+            found |=  t;
         }
-        return set.size() == 26;
+        return found + 1 == all;
     }
+    //     int n = sentence.length();
+    //     HashSet<Character> set = new HashSet<>();
+    //     for(int i = 0 ; i < n ; i++) {
+    //         set.add(sentence.charAt(i));
+    //         if(set.size() == 26) return true;
+    //     }
+    //     return set.size() == 26;
+    // }
 }
