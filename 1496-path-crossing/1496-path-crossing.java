862b98edb5f7bf1553f29a11c68bdd10e8289149
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-19 13:15:57 +0300

    Time: 1 ms (93.40%), Space: 40.7 MB (97.87%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isPathCrossing(String path) {
         int n = path.length();
-        HashSet<String> set = new HashSet<>();
-        int x = 0;
-        int y = 0;
-        set.add(x + "," + y);
+        HashSet<Pair<Integer , Integer>> set = new HashSet<>();
+        Pair<Integer , Integer> pair = new Pair(0 , 0);
+        set.add(pair);
         for(int i = 0 ; i < n ; i++){
             char c = path.charAt(i);
+            int x = pair.getKey();
+            int y = pair.getValue();
             if(c == 'N') y++;
             else if(c == 'S') y--;
             else if(c == 'W') x--;
             else x++;
-            String s = x + "," + y;
-            if(set.contains(s)) return true;
-            set.add(s);
+            Pair<Integer , Integer> p = new Pair(x , y);
+            if(set.contains(p)) return true;
+            set.add(p);
+            pair = p;
         }
         return false;
     }
