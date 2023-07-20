5c0abbf46b88e5e13d2e16930ea8aa3efe00590a
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-20 12:47:56 +0300

    Time: 6 ms (47.97%), Space: 43.7 MB (92.19%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,35 +1,62 @@
 class Solution {
+    private int size;
+    private List<List<String>> solutions = new ArrayList<List<String>>();
+    
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> r = new ArrayList<>();
-        backtrack(n , r , new ArrayList<>() , 0 , new HashSet<>() , new HashSet<>() , new HashSet<>());
-        return r;
-    }
-    public void backtrack(int n , List<List<String>> r , List<String> curr , int row , HashSet<Integer> column , HashSet<Integer> diag , HashSet<Integer> anti){
-        if(n == curr.size()){
-            r.add(new ArrayList<>(curr));
-            return;
-        }
-        for(int i = 0 ; i < n ; i++){
-            if(!column.contains(i) && !diag.contains(row - i) && !anti.contains(row + i)){
-                String s = generateQueen(n , i);
-                curr.add(s);
-                column.add(i);
-                diag.add(row - i);
-                anti.add(row + i);
-                backtrack(n , r , curr , row + 1 , column , diag , anti);
-                anti.remove(row + i);
-                diag.remove(row - i);
-                column.remove(i);
-                curr.remove(curr.size() - 1);
+        size = n;
+        char emptyBoard[][] = new char[size][size];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                emptyBoard[i][j] = '.';
             }
         }
+
+        backtrack(0, new HashSet<>(), new HashSet<>(), new HashSet<>(), emptyBoard);
+        return solutions;
     }
-    public String generateQueen(int n , int j){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < n  ; i++){
-            if(i == j) sb.append("Q");
-            else sb.append(".");
+    
+    // Making use of a helper function to get the
+    // solutions in the correct output format
+    private List<String> createBoard(char[][] state) {
+        List<String> board = new ArrayList<String>();
+        for (int row = 0; row < size; row++) {
+            String current_row = new String(state[row]);
+            board.add(current_row);
         }
-        return sb.toString();
+        
+        return board;
+    }
+    
+    private void backtrack(int row, Set<Integer> diagonals, Set<Integer> antiDiagonals, Set<Integer> cols, char[][] state) {
+        // Base case - N queens have been placed
+        if (row == size) {
+            solutions.add(createBoard(state));
+            return;
+        }
+        
+        for (int col = 0; col < size; col++) {
+            int currDiagonal = row - col;
+            int currAntiDiagonal = row + col;
+            // If the queen is not placeable
+            if (cols.contains(col) || diagonals.contains(currDiagonal) || antiDiagonals.contains(currAntiDiagonal)) {
+                continue;    
+            }
+            
+            // "Add" the queen to the board
+            cols.add(col);
+            diagonals.add(currDiagonal);
+            antiDiagonals.add(currAntiDiagonal);
+            state[row][col] = 'Q';
+
+            // Move on to the next row with the updated board state
+            backtrack(row + 1, diagonals, antiDiagonals, cols, state);
+
+            // "Remove" the queen from the board since we have already
+            // explored all valid paths using the above function call
+            cols.remove(col);
+            diagonals.remove(currDiagonal);
+            antiDiagonals.remove(currAntiDiagonal);
+            state[row][col] = '.';
+        }
     }
 }
