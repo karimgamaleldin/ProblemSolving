197e2d82903c3b9f34f7930cf8545af646e89f68
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-21 14:54:43 +0300

    Time: 141 ms (23.55%), Space: 44.5 MB (40.15%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,40 @@
+class Solution {
+    Map<Character , Integer> map1 = new HashMap<>();
+    Map<Character , Integer> map2 = new HashMap<>();
+    public String minWindow(String s, String t){
+        int m = s.length();
+        int n = t.length();
+        if(n > m) return "";
+        for(int i = 0 ; i < n ; i++) map2.put(t.charAt(i) , map2.getOrDefault(t.charAt(i) , 0) + 1);
+        int left = 0;
+        int right = 0;
+        int num = 0;
+        int min = m;
+        String r = "";
+        while(left < m){
+            if(num == n){
+                if(right - left <= min){
+                    r = s.substring(left , right);
+                    min = right - left;
+                }
+                char c = s.charAt(left);
+                if(map2.containsKey(c)){
+                    if(map1.get(c) <= map2.get(c)){
+                        num--;
+                    }
+                    map1.put(c , map1.get(c) - 1);
+                }
+                left++;
+            }else{
+                if(right >= m) break;
+                char c = s.charAt(right);
+                if(map2.containsKey(c)){
+                    if(map1.getOrDefault(c , 0) < map2.get(c)) num++;
+                    map1.put(c , map1.getOrDefault(c , 0) + 1);
+                }
+                right++;
+            }
+        }
+        return r;
+    }
+}
