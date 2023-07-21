b22f4b9531f43158f3e8891b295b4c61847f9a46
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-21 04:26:40 +0300

    Time: 1 ms (83.61%), Space: 40.9 MB (44.39%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,19 +1,18 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        StringBuilder ss = new StringBuilder();
-        StringBuilder st = new StringBuilder();
-        for(int i = 0 ; i < n ; i++){
-            char x = s.charAt(i);
-            if(x != '#') ss.append(x);
-            else if(ss.length() != 0) ss.deleteCharAt(ss.length() - 1);
+        return build(s).equals(build(t));
+    }
+    
+    public String build(String s) {
+        StringBuilder stack = new StringBuilder();
+        for (char c: s.toCharArray()) {
+            if (c != '#') {
+                stack.append(c);
+            } else if (stack.length() > 0) {
+                stack.deleteCharAt(stack.length() - 1);
+            }
         }
-        for(int i = 0 ; i < m ; i++){
-            char x = t.charAt(i);
-            if(x != '#') st.append(x);
-            else if(st.length() != 0) st.deleteCharAt(st.length() - 1);
-        }   
-        return ss.toString().equals(st.toString());
+        
+        return stack.toString();
     }
 }
