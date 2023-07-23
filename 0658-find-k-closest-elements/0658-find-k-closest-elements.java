bde90fe86c2599eee585e57707bbc0bdef2213d3
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-23 19:02:06 +0300

    Time: 81 ms (5.40%), Space: 45.4 MB (26.59%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,37 +1,53 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        LinkedList<Integer> res = new LinkedList<>();
-        int i = 0;
-        for(; i < arr.length ; i++){
-            if(arr[i] == x){
-                break;
-            }else if (arr[i] > x){
-                i--;
-                break;
-            }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a , b) -> checkCloserI(a , b , x));
+        for(int num: arr){
+            pq.add(num);
+            if(pq.size() > k) pq.remove();
         }
-        if(i == arr.length) i--;
-        int left = i;
-        int right = i+1;
-        while(right - left  <= k){
-            if(right < arr.length && left >= 0){
-                if(checkCloser(arr[left] , arr[right] , x)){
-                    res.addFirst(arr[left--]);
-                }else{
-                    res.addLast(arr[right++]);
-                }
-            }
-            else if(right >= arr.length){
-                res.addFirst(arr[left--]);
-            }
-            else if(left < 0){
-                res.addLast(arr[right++]);
-            }
+        List<Integer> ans = new ArrayList<>();
+        while(!pq.isEmpty()){
+            ans.add(pq.remove());
         }
-        return res;
+        Collections.sort(ans);
+        return ans;
     }
+    //     LinkedList<Integer> res = new LinkedList<>();
+    //     int i = 0;
+    //     for(; i < arr.length ; i++){
+    //         if(arr[i] == x){
+    //             break;
+    //         }else if (arr[i] > x){
+    //             i--;
+    //             break;
+    //         }
+    //     }
+    //     if(i == arr.length) i--;
+    //     int left = i;
+    //     int right = i+1;
+    //     while(right - left  <= k){
+    //         if(right < arr.length && left >= 0){
+    //             if(checkCloser(arr[left] , arr[right] , x)){
+    //                 res.addFirst(arr[left--]);
+    //             }else{
+    //                 res.addLast(arr[right++]);
+    //             }
+    //         }
+    //         else if(right >= arr.length){
+    //             res.addFirst(arr[left--]);
+    //         }
+    //         else if(left < 0){
+    //             res.addLast(arr[right++]);
+    //         }
+    //     }
+    //     return res;
+    // }
     public boolean checkCloser(int a , int b , int x){
         if(Math.abs(a-x) < Math.abs(b-x) || (Math.abs(a-x) == Math.abs(b-x) && a <= b)) return true;
         return false;
+    }
+    public int checkCloserI(int a , int b , int x){
+        if(Math.abs(a-x) < Math.abs(b-x) || (Math.abs(a-x) == Math.abs(b-x) && a <= b)) return 1;
+        return -1;
     }
 }
