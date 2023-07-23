306222dade4b2c175a25a1872b22445b3f39598f
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-23 18:52:02 +0300

    Time: 15 ms (54.74%), Space: 48 MB (21.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,18 +1,11 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer , Integer> map = new HashMap<Integer , Integer>();
-        for(int i = 0 ; i < nums.length ; i++){
-            map.computeIfPresent(nums[i] , (key , value) -> value+1);
-            if(!map.containsKey(nums[i])) map.put(nums[i] , 1);
-        }
-        int[] r = new int[k]; 
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>( (a, b) -> b.getValue() - a.getValue());       
-        for(Map.Entry<Integer, Integer> ele: map.entrySet()){
-            pq.add(ele);
-        }
-        for(int i = 0 ; i < k ; i++){
-            r[i] = pq.poll().getKey();
-        }
+        Map<Integer , Integer> map = new HashMap<>();
+        for(int num  : nums) map.put(num , map.getOrDefault(num , 0) + 1);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        for(int num : map.keySet()) pq.add(num);
+        int[] r = new int[k];
+        for(int i = 0 ; i < k ; i++) r[i] = pq.remove();
         return r;
     }
 }
