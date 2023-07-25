be777ac7adf536348b96009805d211d0a3c4f884
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-25 13:09:30 +0300

    Time: 3 ms (65.95%), Space: 41 MB (85.95%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] numsSameConsecDiff(int n, int k) {
         List<Integer> r = new LinkedList<>();
-        backtrack(r , 1 , 0 , n , k , true);
+        for(int i = 1 ; i <= 9 ; i++)
+            backtrack(r , i , n - 1 , k);
         int[] arr = new int[r.size()];
         for(int i = 0 ; i < r.size() ; i++) arr[i] = r.get(i);
         return arr;
     }
-    public void backtrack(List<Integer> r , int start , int currNum , int n , int k , boolean first){
+    public void backtrack(List<Integer> r , int currNum , int n , int k){
         if(n == 0){
             r.add(currNum);
             return;
         }
         int num = currNum % 10;
-        for(int i = start ; i <= 9 ; i++){
+        for(int i = 0 ; i <= 9 ; i++){
             int a = i < num ? num - i : i - num;
-            if(first || a == k){
+            if(a == k){
                 int b = i + currNum * 10;
-                backtrack(r , 0 , b , n - 1 , k , false);
+                backtrack(r , b , n - 1 , k);
             }
         }
     }
