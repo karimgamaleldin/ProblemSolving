8b2c93858b3a1a32769e8beea6b2cc36824c2001
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-30 23:58:59 +0300

    Time: 3 ms (64.94%), Space: 42 MB (16.63%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,30 +1,44 @@
 class Solution {
+    HashMap<Pair<Integer, Integer>, Boolean> memo;
+    int n;
+    int m;
+    String s;
+    String p;
 
     public boolean isMatch(String s, String p) {
-        boolean[][] cache = new boolean[s.length() + 1][p.length() + 1];
-
-        return dfs(cache, s, p, 0, 0);
+        this.n = s.length();
+        this.m = p.length();
+        this.s = s;
+        this.p = p;
+        this.memo = new HashMap<>();
+        return dp(0, 0);
     }
 
-    private boolean dfs(boolean[][] cache, String s, String p, int i, int j) {
-        if (cache[i][j] != false) return cache[i][j];
-
-        if (i >= s.length() && j >= p.length()) return true;
-
-        if (j >= p.length()) return false;
-
-        boolean match =
-            i < s.length() &&
-            (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
-
-        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
-            cache[i][j] =
-                dfs(cache, s, p, i, j + 2) ||
-                (match && dfs(cache, s, p, i + 1, j));
-        } else {
-            cache[i][j] = match && dfs(cache, s, p, i + 1, j + 1);
+    public boolean dp(int i, int j) {
+        Pair<Integer, Integer> key = new Pair<>(i, j);
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
-
-        return cache[i][j];
+        if (i >= n) {
+            // Check if there are remaining characters in the regular expression
+            while (j + 1 < m && p.charAt(j + 1) == '*') {
+                j += 2;
+            }
+            // If there are no remaining characters, it's a match; otherwise, return false
+            return j == m;
+        }
+        if (j >= m) return false; // we finished the regular expression and there is no match
+        boolean f = i < n && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+        if (j + 1 < m && p.charAt(j + 1) == '*') {
+            boolean t = dp(i, j + 2) || (f && dp(i + 1, j));
+            memo.put(key, t);
+            return t;
+        } else if (f) {
+            boolean t = dp(i + 1, j + 1);
+            memo.put(key, t);
+            return t;
+        }
+        memo.put(key, false);
+        return false;
     }
 }
