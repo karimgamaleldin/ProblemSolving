97f4f824899267cc01ac0b36be75f567fa87d6c6
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-07-31 00:48:52 +0300

    Time: 4 ms (44.35%), Space: 42.9 MB (43.87%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,24 +1,25 @@
 class Solution {
-    // HashMap<Pair<Integer , Integer> , Boolean> memo;
-    // int[] arr;
+    HashMap<Pair<Integer , Integer> , Boolean> memo;
+    int[] arr;
     public boolean stoneGame(int[] piles) {
-        return true;
-        // this.arr = piles;
-        // this.memo = new HashMap<>();
-        // return dp(0 , piles.length - 1, 0 , 0);
+        this.arr = piles;
+        this.memo = new HashMap<>();
+        boolean t = dp(0 , piles.length - 1, 0 , 0);
+        // System.out.println(memo);
+        return t;
     }
-//     public boolean dp(int i , int j , int Alice , int Bob){
-//         Pair<Integer , Integer> key = new Pair(i,j);
-//         if(memo.containsKey(key)){
-//             return memo.get(key);
-//         }
-//         if(i > j) return false;
-//         if(i == j){
-//             return Alice > Bob;
-//         }
-//         boolean t = dp(i+1,j-1,Alice + arr[i] , Bob+arr[j]) || dp(i+1,j-1,Alice + arr[j] , Bob+arr[i]) || dp(i+2,j,Alice + arr[i] , Bob+arr[i+1]) || dp(i,j-2,Alice + arr[j] , Bob+arr[j-1]);
-//         memo.put(key , t);
-//         return t;
-//     }
+    public boolean dp(int i , int j , int Alice , int Bob){
+        // System.out.println(i + "," + j);
+        Pair<Integer , Integer> key = new Pair(i,j);
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+        if(i >= j){
+            return Alice > Bob;
+        }
+        boolean t = dp(i+1,j-1,Alice + arr[i] , Bob+arr[j]) || dp(i+1,j-1,Alice + arr[j] , Bob+arr[i]) || dp(i+2,j,Alice + arr[i] , Bob+arr[i+1]) || dp(i,j-2,Alice + arr[j] , Bob+arr[j-1]);
+        memo.put(key , t);
+        return t;
+    }
     
 }
