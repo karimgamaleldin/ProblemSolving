1b1c72c78c82b6a6b8c7feac01d8a018582cf2f7
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-01 01:06:33 +0300

    Time: 2 ms (41.25%), Space: 46 MB (23.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,32 +1,23 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        HashMap<Integer , ArrayList<Integer>> map = new HashMap<>();
-        for(int i = 0 ; i < isConnected.length ; i++){
-            ArrayList<Integer> x = new ArrayList<>();
-            for(int j = 0; j < isConnected[i].length ; j++){
-                if(i == j) continue;
-                if(isConnected[i][j] == 1) x.add(j);
-            }
-            map.put(i,x);
-        }
-        boolean[] seen = new boolean[isConnected.length];
-        int province = 0;
-        for(int i = 0 ; i < isConnected.length ; i++){
-            if(!seen[i]){
-                province++;
-                seen[i] = true;
-                helper(i , map , seen);
+        int n = isConnected.length;
+        HashSet<Integer> seen = new HashSet<>();
+        int count = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(!seen.contains(i)){
+                seen.add(n);
+                count++;
+                dfs(seen , i , isConnected);
             }
         }
-        return province;
-        
-    }       
-    public void helper(int num , HashMap<Integer , ArrayList<Integer>> map , boolean[] seen){
-        ArrayList<Integer> arr = map.get(num);
-        for(int j = 0 ; j < arr.size() ; j++){
-            if(!seen[arr.get(j)]){
-                seen[arr.get(j)] = true;
-                helper(arr.get(j) , map , seen);
+        return count;
+    }
+    public void dfs(HashSet<Integer> seen , int v , int[][] isConnected){
+        int[] arr = isConnected[v];
+        for(int i = 0 ; i < arr.length ; i++){
+            if(!seen.contains(i) && arr[i] == 1){
+                seen.add(i);
+                dfs(seen , i , isConnected);
             }
         }
     }
