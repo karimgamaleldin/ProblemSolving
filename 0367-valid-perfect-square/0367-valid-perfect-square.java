958c517a7b82f787f860b930b7947e2efe9949d8
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-07 23:54:19 +0300

    Time: 0 ms (100.00%), Space: 39.4 MB (48.43%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(num == 1) return true;
-        long min = 1;
-        long max = num / 2;
-        while(min <= max){
-            long mid = (max + min) / 2;
-            if(mid*mid == num) return true;
-            else if(mid*mid < num) min = mid + 1;
-            else max = mid - 1;
+        if(num < 2) return true;
+        long l = 1;
+        long r = num / 2;
+        while(l <= r){
+            long mid = l + (r - l) / 2;
+            long x = mid * mid;
+            if(x == num){
+                return true;
+            }else if(x > num){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
         }
         return false;
-        
     }
 }
