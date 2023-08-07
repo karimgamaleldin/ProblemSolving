a8e87a6b8246c876230619ce0f976a2e0567e4f3
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-07 23:45:09 +0300

    Time: 0 ms (100.00%), Space: 40.2 MB (98.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,28 @@
 class Solution {
-    public double myPow(double x, int n) {
-        return Math.pow(x , n);
+     public double myPow(double x, int n) {
+        return myPowH(x, (long) n);
     }
+    public double myPowH(double x, long n) {
+        if(n == 0) return 1;
+        double solution = 1;
+        if(n < 0){
+            n *= -1;
+            x = 1 / x;
+        }
+        while(n != 0){
+            if(n % 2 == 1){
+                solution = solution * x;
+                n--;
+            }
+            n /= 2;
+            x = x * x;
+        }
+        return solution;
+    }
+    // public double helper(double x , int n){
+    //     if(n == 0) return 1;
+    //     if(n < 0) return 1.0 / helper(x , -1 * n);
+    //     if(n % 2 == 1) return x * helper(x * x , (n - 1) / 2);
+    //     return helper(x * x , n / 2);
+    // }
 }
