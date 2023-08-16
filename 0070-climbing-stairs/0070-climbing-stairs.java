4e65123007a581a34839191b1a666df4ec999ef5
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-16 13:39:40 +0300

    Time: 0 ms (100.00%), Space: 38.6 MB (99.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,16 @@
 class Solution {
+    // Memoization
     public int climbStairs(int n) {
+        int[] memo = new int[n + 1];
+        return dp(n , memo);
+    }
+    public int dp(int n , int[] memo){
+        if(n <= 0) return 0;
         if(n == 1) return 1;
-        int memo1 = 1;
-        int memo2 = 1;
-        for(int i = n - 2; i >= 0 ;i--){
-            int temp = memo1;
-            memo1 = memo1 + memo2;
-            memo2 = temp;
-        }
-        return memo1;
+        if(n == 2) return 2;
+        if(memo[n] != 0) return memo[n];
+        int x = dp(n-1 , memo) + dp(n-2 , memo);
+        memo[n] = x;
+        return x;
     }
 }
