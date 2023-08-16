8db9ea3b1e16b84413927faabf271c04293461a8
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-16 14:36:32 +0300

    Time: 1 ms (54.67%), Space: 42.7 MB (95.32%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,26 +1,15 @@
 class Solution {
-    // Map<Integer , Integer> memo = new HashMap<>();
-    // int[] arr;
     public int minCostClimbingStairs(int[] cost) {
-        int[] tab = new int[cost.length + 1];
-        for(int i = 2 ; i < tab.length ; i++){
-            tab[i] = Math.min(cost[i - 1] + tab[i - 1] , cost[i - 2] + tab[i - 2]);
-        }
-        return tab[tab.length - 1];
+        int n = cost.length;
+        int[] memo = new int[n];
+        dp(0 , cost , memo);
+        return Math.min(memo[0] , memo[1]);
     }
-//     public int minCostClimbingStairsBottomUp(int[] cost) {
-//         arr = cost;
-//         dp(cost.length);
-//         return memo.get(cost.length);
-//     }
-    
-//     public int dp(int i){
-//         if(i == 0 || i == 1){
-//             return 0;
-//         }
-//         else{
-//             if(!memo.containsKey(i)) memo.put(i , Math.min(dp(i-1) + arr[i-1] , dp(i-2) + arr[i-2]));
-//             return memo.get(i);
-//         }
-//     }
+    public int dp(int n , int[] cost , int[] memo){
+        if(n >= cost.length) return 0;
+        if(memo[n] != 0) return memo[n];
+        int c = cost[n] + Math.min(dp(n + 1 , cost , memo) , dp(n + 2 , cost , memo));
+        memo[n] = c;
+        return c;
+    }
 }
