3178bbda630393aaedb1a7a38648e29b388fd563
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-16 18:17:08 +0300

    Time: 20 ms (92.37%), Space: 48.1 MB (80.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,27 +1,40 @@
 class Solution {
-    HashMap<String , Integer> memo = new HashMap<>();
-    String s1;
-    String s2;
     public int longestCommonSubsequence(String text1, String text2) {
-        this.s1 = text1;
-        this.s2 = text2;
-        return dp(0 , 0);
-    }
-    public int dp(int i , int j){
-        if(i >= s1.length() || j >= s2.length()) return 0;
-        else if(memo.containsKey(keyGetter(i , j))) return memo.get(keyGetter(i , j));
-        char c = s1.charAt(i);
-        char d = s2.charAt(j);
-        int t = 0;
-        if(c == d){
-            t = dp(i + 1 , j + 1) + 1;
+        int n = text1.length();
+        int m = text2.length();
+        int[][] tab = new int[n + 1][m + 1];
+        for(int i = n - 1 ; i >= 0 ;i--){
+            for(int j = m - 1 ; j >= 0 ; j--){
+                char c = text1.charAt(i) , d = text2.charAt(j);
+                if(c == d) tab[i][j] = 1 + tab[i + 1][j + 1];
+                else tab[i][j] = Math.max(tab[i + 1][j] , tab[i][j + 1]);
+            }
         }
-        else t = Math.max(dp(i + 1 , j) , Math.max(dp(i + 1 , j + 1) , dp(i , j + 1)));
-        memo.put(keyGetter(i , j) , t);
-        return t;
+        return tab[0][0];
     }
-    public String keyGetter(int i , int j){
-        return i + " " + j;
-    }
+    // HashMap<String , Integer> memo = new HashMap<>();
+    // String s1;
+    // String s2;
+    // public int longestCommonSubsequence(String text1, String text2) {
+    //     this.s1 = text1;
+    //     this.s2 = text2;
+    //     return dp(0 , 0);
+    // }
+    // public int dp(int i , int j){
+    //     if(i >= s1.length() || j >= s2.length()) return 0;
+    //     else if(memo.containsKey(keyGetter(i , j))) return memo.get(keyGetter(i , j));
+    //     char c = s1.charAt(i);
+    //     char d = s2.charAt(j);
+    //     int t = 0;
+    //     if(c == d){
+    //         t = dp(i + 1 , j + 1) + 1;
+    //     }
+    //     else t = Math.max(dp(i + 1 , j) , Math.max(dp(i + 1 , j + 1) , dp(i , j + 1)));
+    //     memo.put(keyGetter(i , j) , t);
+    //     return t;
+    // }
+    // public String keyGetter(int i , int j){
+    //     return i + " " + j;
+    // }
     
 }
