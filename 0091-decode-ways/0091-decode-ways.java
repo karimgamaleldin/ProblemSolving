2114d7cbc2d21dbd630eed41901a815dfaa32ed9
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-19 18:38:01 +0300

    Time: 1 ms (91.62%), Space: 41 MB (40.53%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,22 +1,19 @@
 class Solution {
-    /*
-    Let's assume we take the first character
-    we can take it or leave it
-    then we can build on the the previous values by either taking it or leaving it.
-    */
     HashMap<Integer , Integer> memo = new HashMap<>();
+    String s;
     public int numDecodings(String s) {
         if(s.length() == 0) return 0;
-        return dp(s , 0);
+        this.s = s;
+        return dp(0);
     }
-    public int dp(String s , int i){
-        if(memo.containsKey(i)) return memo.get(i);
+    public int dp(int i){
         if(i == s.length()) return 1;
-        if(s.charAt(i) == '0') return 0;
-        if(i == s.length() - 1) return 1;
-        int a = dp(s , i + 1); // take the next
-        int b = Integer.parseInt(s.substring(i , i + 2)) < 27 ? dp(s , i + 2) : 0 ; // skip one point
-        memo.put(i , a + b);
-        return a + b;
+        if(s.charAt(i) == '0') return 0; 
+        if(memo.containsKey(i)) return memo.get(i);
+        int a = dp(i + 1);
+        int b = i < s.length() - 1 && Integer.parseInt(s.substring(i , i+2)) < 27 ? dp(i + 2) : 0;
+        int x = a + b;
+        memo.put(i , x);
+        return x;
     }
 }
