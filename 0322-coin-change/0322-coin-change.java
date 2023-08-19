ad23e8da56f8e792a371099dd680a60b346a5ed6
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-19 12:40:28 +0300

    Time: 16 ms (74.42%), Space: 42.7 MB (96.48%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,15 +1,19 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] tab = new int[amount + 1];
+        int size = amount + 1;
+        int[] tab = new int[size];
+        tab[0] = 0;
+        for(int i = 0 ; i < coins.length && coins[i] <= amount ; i++) tab[coins[i]] = 1;
         for(int i = 1 ; i <= amount ; i++){
-            tab[i] = amount + 1;
-            for(int j = 0 ; j < coins.length ; j++){
-                int k = i - coins[j];
-                if(k < 0) continue;
-                int temp = 1 + tab[k];
-                tab[i] = Math.min(temp , tab[i]); 
+            tab[i] = Integer.MAX_VALUE; // To know if not possible
+            for(int coin : coins){
+                int x = i - coin;
+                if(x < 0) continue;
+                if(tab[x] != Integer.MAX_VALUE){
+                    tab[i] = Math.min(tab[x] + 1 , tab[i]);
+                }
             }
         }
-        return tab[amount] != amount + 1 ? tab[amount] : -1;
+        return tab[amount] != Integer.MAX_VALUE ? tab[amount] : -1;
     }
 }
