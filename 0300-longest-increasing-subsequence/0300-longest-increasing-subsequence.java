602414742316dd509175b478611deaddf22d849a
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-19 13:03:51 +0300

    Time: 38 ms (73.23%), Space: 42.9 MB (92.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,36 +1,14 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            dp[i] = 1;
-            for(int j = 0 ; j < i ; j++){
-                if(nums[i] > nums[j])
-                    dp[i] = Math.max(dp[i] , dp[j] + 1);
+        int[] tab = new int[nums.length];
+        int max = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            for(int j = i - 1; j >= 0 ; j--){
+                if(nums[i] > nums[j]) tab[i] = Math.max(tab[i] , tab[j]);
             }
-            ans = Math.max(dp[i] , ans);
+            tab[i]++;
+            max = Math.max(tab[i] , max);
         }
-        return ans;
+        return max;
     }
-    // HashMap<Integer , Integer> memo = new HashMap<>();
-    // public int lengthOfLIS(int[] nums) {
-    //     int ans = 1;
-    //     for(int i = 0 ; i < nums.length ; i++){
-    //         ans = Math.max(ans, dp(i , nums));
-    //     }
-    //     return ans;
-    // }
-    // public int dp(int i , int[] arr){
-    //     if(memo.containsKey(i)){
-    //         return memo.get(i);
-    //     }
-    //     int ans = 1;
-    //     for(int j = 0 ; j < i ; j++){
-    //         if(arr[j] < arr[i])
-    //             ans = Math.max(ans , dp(j , arr) + 1);
-    //     }
-    //     memo.put(i , ans);
-    //     return ans;
-    // }
 }
