248a549679d63393ca44c5199dfdb64990bed9da
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-19 13:30:02 +0300

    Time: 1 ms (84.91%), Space: 40.1 MB (97.26%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,23 +1,28 @@
 class Solution {
     int[][] memo;
+    int[] prices;
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        this.memo = new int[n][3];
-        for(int i = 0 ; i < n ; i++) Arrays.fill(memo[i] , Integer.MIN_VALUE);
-        return dp(prices , 0 , 0);
+        this.memo = new int[prices.length][3]; // three states : sell , cooldown , buy
+        for(int[] m : memo) Arrays.fill(m , -1);
+        this.prices = prices;
+        return dp(0 , 0);
     }
-    public int dp(int[] prices , int i , int state){
+    public int dp(int i , int state){
         if(i == prices.length) return 0;
-        if(memo[i][state] != Integer.MIN_VALUE) return memo[i][state];
-        int res = 0;
+        if(memo[i][state] != -1) return memo[i][state];
+        int r = 0;
         if(state == 0){
-            res = Math.max(dp(prices , i + 1 , 1) - prices[i] , dp(prices , i + 1 , 0));
+            int buy = dp(i + 1 , 1) - prices[i];
+            int rem = dp(i + 1 , 0);
+            r = Math.max(buy , rem);
         }else if(state == 1){
-            res = Math.max(dp(prices , i + 1 , 2) + prices[i] , dp(prices , i + 1 , 1));
+            int sell = dp(i + 1 , 2) + prices[i];
+            int keep = dp(i + 1 , 1);
+            r = Math.max(sell , keep);
         }else{
-            res = dp(prices , i + 1 , 0);
+            r = dp(i + 1 , 0);
         }
-        memo[i][state] = res;
-        return res;
+        memo[i][state] = r;
+        return r;
     }
 }
