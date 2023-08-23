0cc81d133b7e3240daaa4b4c445820cd425b2705
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-23 20:12:56 +0300

    Time: 30 ms (89.56%), Space: 51.1 MB (45.80%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        // similar to 2 sum but we need a target to be 0 and we will make an extra loop
         Arrays.sort(nums);
-        List<List<Integer>> r = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         for(int i = 0 ; i < nums.length - 2 ; i++){
-            if(i==0 || (i>0 && nums[i] != nums[i-1])){ //the idea behind is that we can have duplicates of the same value
-                int j = i + 1;
-                int k = nums.length - 1;
-                while(j < k){
-                    int x = nums[i] + nums[j] + nums[k];
-                    if(x == 0){
-                        ArrayList<Integer> t = new ArrayList<>();
-                        t.add(nums[i]);
-                        t.add(nums[j]);
-                        t.add(nums[k]);
-                        r.add(t);
-                        while(j < k && nums[j] == nums[j+1]) j++;
-                        while(k > j && nums[k] == nums[k-1]) k--;
-                        j++;
-                        k--;
-                    }
-                    else if( x < 0 ) j++;
-                    else k--;
-                }
+            if(i != 0 && nums[i - 1] == nums[i]) continue;
+            int target = 0 - nums[i];
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j < k){
+                int x = nums[j] + nums[k];
+                if(target == x){
+                    List<Integer> l = new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    l.add(nums[k]);
+                    ans.add(l);
+                    while(j < k && nums[j] == nums[j+1]) j++;
+                    while(k > j && nums[k] == nums[k-1]) k--;
+                    k--;
+                    j++;
+                }else if(x > target) k--;
+                else j++;
             }
         }
-        return r;
+        return ans;
     }
 }
