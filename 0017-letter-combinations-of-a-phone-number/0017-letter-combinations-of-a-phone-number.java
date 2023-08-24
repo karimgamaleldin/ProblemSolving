3774e7b818428ff0f22001d9fcfdfa2f624ff10a
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-24 23:48:13 +0300

    Time: 0 ms (100.00%), Space: 40.9 MB (93.64%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,23 +1,22 @@
 class Solution {
     char[][] s = new char[][]{{'a','b','c'} , {'d','e','f'} , {'g','h','i'} ,{'j','k','l'} , {'m','n','o'} , {'p','q','r','s'} , {'t','u','v'} , {'w','x','y','z'}} ;
     public List<String> letterCombinations(String digits) {
-        List<String> r = new ArrayList<>();
-         if(digits.length() == 0) return r;
+        List<String> ans = new ArrayList<>();
+        if(digits.length() == 0) return ans;
         StringBuilder sb = new StringBuilder();
-        backtrack(r , sb , digits , 0);
-        return r;
+        backtrack(ans , sb , 0 , digits);
+        return ans;
     }
-    public void backtrack(List<String> r , StringBuilder sb , String  digits , int index){
-        if(sb.length() == digits.length() || index == digits.length()){
-            r.add(sb.toString());
+    public void backtrack(List<String> ans , StringBuilder sb , int i , String digits){
+        if(i == digits.length()){
+            ans.add(sb.toString());
             return;
         }
-        int button = Integer.parseInt("" + digits.charAt(index));
-        char[] mapping = s[button - 2];
-        for(int i = 0 ; i < mapping.length ; i++){
-            sb.append(mapping[i]);
-            backtrack(r , sb , digits , index + 1);
-            sb.delete(sb.length() - 1 , sb.length());
+        char[] chars = s[digits.charAt(i) - '2'];
+        for(char c : chars){
+            sb.append(c);
+            backtrack(ans , sb , i + 1 , digits);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
