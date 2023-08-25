1ca4a1c72dcb993273f166edf7a0ee047a75fa96
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-25 15:22:26 +0300

    Time: 2 ms (50.45%), Space: 46.6 MB (8.47%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -18,21 +18,22 @@ class Solution {
             for(int i = 0 ; i < size ; i++) root[i] = i;
         }
         
-        public int find(int x){ return root[x]; }
+        public int find(int x){ 
+            while(x != root[x]) x = root[x];
+            return x;
+        }
         
         public void union(int x , int y){
             int rootX = find(x);
             int rootY = find(y);
-            for(int i = 0 ; rootX != rootY && i < root.length ; i++){
-                if(rootY == root[i]) root[i] = root[x];
-            }
+            if(rootX != rootY) root[rootY] = rootX;
         }
         
         public boolean connected(int x , int y){return find(x) == find(y);}
         
         public int numberOfComponents(){
             HashSet<Integer> s = new HashSet<>();
-            for(int i = 0 ; i < root.length ; i++) s.add(root[i]);
+            for(int i = 0 ; i < root.length ; i++) s.add(find(root[i]));
             return s.size();
         }
     }
