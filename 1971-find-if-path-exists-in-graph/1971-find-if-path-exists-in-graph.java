68302444b9dc97aa11bbf301c2464797ba39d16b
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-26 10:42:50 +0300

    Time: 227 ms (21.48%), Space: 211.4 MB (5.07%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,27 +1,26 @@
 class Solution {
-    HashMap<Integer , List<Integer>> map = new HashMap<>();
-    boolean[] seen;
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        seen = new boolean[n];
-        for(int i = 0 ; i < n ; i++) map.put(i , new ArrayList<>());
-        for(int i = 0 ; i < edges.length ; i++){
-            int a = edges[i][0];
-            int b = edges[i][1];
-            map.get(a).add(b);
-            map.get(b).add(a);
+        HashMap<Integer , List<Integer>> map = new HashMap<>();
+        for(int[] edge : edges){
+            if(!map.containsKey(edge[0])) map.put(edge[0] , new ArrayList<>());
+            map.get(edge[0]).add(edge[1]);
+            if(!map.containsKey(edge[1])) map.put(edge[1] , new ArrayList<>());
+            map.get(edge[1]).add(edge[0]);
         }
-        seen[source] = true;
-        dfs(source);
-        return seen[destination];
+        return dfs(map , source , destination , new HashSet<>());
     }
-    public void dfs(int n){
-        List<Integer> arr= map.get(n);
-        for(int i = 0 ; i < arr.size() ; i++){
-            int a = arr.get(i);
-            if(!seen[a]){
-                seen[a] = true;
-                dfs(a);
+    
+    public boolean dfs(HashMap<Integer , List<Integer>> map , int source , int destination , HashSet<Integer> seen){
+        if(source == destination) return true;
+        boolean f = false;
+        List<Integer> m = map.get(source);
+        for(Integer i : m){
+            if(f) break;
+            if(!seen.contains(i)){
+                seen.add(i);
+                f = dfs(map , i , destination , seen);
             }
-        } 
+        }
+        return f;
     }
 }
