d737e303f5286dd7bd1474c25351402d03b19877
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-26 14:56:23 +0300

    Time: 17 ms (63.24%), Space: 44.6 MB (81.20%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,30 +1,27 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int min = 1;
-        int n = grid.length;
         if(grid[0][0] == 1) return -1;
-        if(n == 1) return 1;
-        boolean[][] seen= new boolean[n][n];
-        int[][] directions = new int[][]{{0,1} , {1,0} , {1,1} , {-1,0} , {0,-1} , {-1,-1} , {1 , -1} , {-1 , 1}};
+        int n = grid.length;
+        int[][] dir = new int[][]{{0 , 1} , {1 , 0} , {-1 , 0} , {0 , -1} , {1 , 1} , {-1 , -1} , {-1 , 1} , {1 , -1}};
+        boolean[][] seen = new boolean[n][n];
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0});
+        q.add(new int[]{0 , 0});
+        int ans =0;
         while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0 ; i < size ; i++){
+            ans++;
+            int s = q.size();
+            for(int i = 0 ; i < s ; i++){
                 int[] t = q.remove();
-                int a = t[0];
-                int b = t[1];
-                for(int[] dir: directions){
-                    int ni = a + dir[0];
-                    int nj = b + dir[1];
+                if(t[0] == n - 1 && t[1] == n - 1) return ans;
+                for(int[] d : dir){
+                    int ni = d[0] + t[0];
+                    int nj = d[1] + t[1];
                     if(ni >= 0 && nj >= 0 && ni < n && nj < n && grid[ni][nj] == 0 && !seen[ni][nj]){
                         seen[ni][nj] = true;
-                        if(ni == n - 1 && nj == n - 1){System.out.println('h'); return min + 1;}
-                        q.add(new int[]{ni , nj});
+                        q.add(new int[] {ni , nj});
                     }
                 }
             }
-            min++;
         }
         return -1;
     }
