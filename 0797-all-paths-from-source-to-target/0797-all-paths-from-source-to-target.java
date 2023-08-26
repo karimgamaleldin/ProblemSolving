25742a4f7781617361b3de92890c264d13b55d42
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-26 11:04:51 +0300

    Time: 1 ms (99.75%), Space: 45.1 MB (94.90%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> r = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>(); // graph is acyclic we don't need a seen
         curr.add(0);
-        backtrack(r , curr , graph , 0);
-        return r;
+        bt(res , curr , graph , 0);
+        return res;
     }
-    public void backtrack(List<List<Integer>> r , List<Integer> curr , int[][] graph , int i){
-        if(curr.size() > 0 && curr.get(curr.size() - 1) == graph.length - 1){
-            r.add(new ArrayList<>(curr));
+    public void bt(List<List<Integer>> res , List<Integer> curr , int[][] graph , int i){
+        if(i == graph.length - 1){
+            res.add(new ArrayList<>(curr));
             return;
         }
-        for(int k = 0 ; k < graph[i].length ; k++){
-            curr.add(graph[i][k]);
-            backtrack(r , curr , graph , graph[i][k]);
+        int[] g = graph[i];
+        for(int k : g){
+            curr.add(k);
+            bt(res , curr , graph , k);
             curr.remove(curr.size() - 1);
         }
     }
