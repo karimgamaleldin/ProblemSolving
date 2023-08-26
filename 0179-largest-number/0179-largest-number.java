ed8a43829d30606d11f5e057bd7568857d8a36b3
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-08-26 07:04:59 +0300

    Time: 6 ms (46.10%), Space: 42.8 MB (81.78%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,25 +1,17 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        HashMap<Integer , PriorityQueue<String>> map = new HashMap<>();
-        for(int num : nums){
-            String s = num + "";
-            int x = s.charAt(0) - '0';
-            if(!map.containsKey(x)) map.put(x , new PriorityQueue<>((a , b) -> {
-                String z = a + b;
-                String y = b + a;
-                return y.compareTo(z);
-                
-            }));
-            map.get(x).add(s);
+        String[] arr = new String[nums.length];
+        for(int i = 0 ; i < nums.length ; i++){
+            arr[i] = "" + nums[i];
         }
-        
+        Arrays.sort(arr, (a , b) -> {
+            String x = a + b;
+            String y = b + a;
+            return y.compareTo(x);
+        });
         StringBuilder sb = new StringBuilder();
-        for(int i = 9 ; i >= 0 ; i--){
-            PriorityQueue<String> pq = map.get(i);
-            if(pq == null) continue;
-            while(!pq.isEmpty()){
-                sb.append(pq.poll());
-            }
+        for(String s : arr){
+            sb.append(s);
         }
         if(sb.charAt(0) == '0') return "0";
         return sb.toString();
