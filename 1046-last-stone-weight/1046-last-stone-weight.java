c308f733ba2a9d43a5a6a0c9ad98ce790e19a6da
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-01 00:59:46 +0300

    Time: 1 ms (98.91%), Space: 39.5 MB (95.16%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,14 +1,15 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a , b) -> b - a);
-        for(int i = 0 ; i < stones.length ; i++) pq.add(stones[i]);
-        while(pq.size() > 1){
-            int y = pq.remove();
-            int x = pq.remove();
-            if(x != y){
-                pq.add(y - x);
-            }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        for(int s : stones){
+            pq.add(s);
         }
-        return pq.isEmpty() ? 0 : pq.remove();
+        while(pq.size() > 1){
+            int x = pq.poll();
+            int y = pq.poll();
+            if(x == y) continue;
+            pq.add(Math.abs(y - x));
+        }
+        return pq.size() == 0 ? 0 : pq.poll(); 
     }
 }
