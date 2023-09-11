390b9ffb36e48180dc5b64627611612c2888c671
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-12 00:02:50 +0300

    Time: 67 ms (24.02%), Space: 44.5 MB (31.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -4,33 +4,34 @@ class Solution {
         Map<Character , Integer> map1 = new HashMap<>();
         Map<Character , Integer> map2 = new HashMap<>();
         for(int i = 0 ; i < n ; i++) map2.put(t.charAt(i) , map2.getOrDefault(t.charAt(i) , 0) + 1);
-        int counts = 0;
-        int j = 0;
-        int distinct = map2.size();
-        int ans = Integer.MAX_VALUE;
-        String r = "";
-        for(int i = 0 ; i < m ; i++){
-            char x = s.charAt(i);
-            map1.put(x , map1.getOrDefault(x , 0) + 1);
-            if(map1.get(x).equals(map2.getOrDefault(x , -1))) counts++;
-            while(counts == distinct){
-                if(i - j + 1 < ans){
-                    ans = i - j + 1;
-                    r = s.substring(j , i + 1);
+        int l = 0;
+        int r = 0;
+        String ans = "";
+        int min = Integer.MAX_VALUE;
+        int count = 0;
+        while(r < m){
+            char c = s.charAt(r);
+            int x = map1.getOrDefault(c , 0) + 1;
+            int y = map2.getOrDefault(c, -1);
+            map1.put(c, x);
+            if(x <= y) count++;
+            while(count == n){
+                if(r - l + 1 < min){
+                    min = r - l + 1;
+                    ans = s.substring(l, r + 1);
                 }
-                char c = s.charAt(j);
-                map1.put(c , map1.get(c) - 1);
-                if(map2.containsKey(c) && map1.get(c) < map2.get(c)){
-                    counts--;
-                }
-                j++;
+                char d = s.charAt(l);
+                if(map2.containsKey(d) && map1.get(d) <= map2.get(d)) count--;
+                map1.put(d, map1.get(d) - 1);
+                l++;
             }
+            r++;
         }
         // System.out.println(map1);
         // System.out.println(map1.get('a'));
         // System.out.println(map2.get('a'));
         // System.out.println(map2);
         // System.out.println(counts +" "+ distinct);
-        return r;
+        return ans;
     }
 }
