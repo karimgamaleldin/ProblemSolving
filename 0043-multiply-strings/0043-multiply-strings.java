c0a7d865edc8331e1200077d6eb20a2288709a47
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-11 20:40:11 +0300

    Time: 3 ms (83.21%), Space: 41.1 MB (81.93%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -2,27 +2,27 @@ class Solution {
     public String multiply(String num1, String num2) {
         int m = num1.length();
         int n = num2.length();
-        int[] product = new int[m + n]; // Store the product of each digit multiplication
+        if(num1.equals("0") || num2.equals("0")) return "0";
+        int[] product = new int[m + n]; 
         
-        // Perform the digit multiplication and addition
         for (int i = m - 1; i >= 0; i--) {
             int x = num1.charAt(i) - '0';
             for (int j = n - 1; j >= 0; j--) {
                 int y = num2.charAt(j) - '0';
-                int temp = x * y + product[i + j + 1]; // Add the previous product value
-                product[i + j + 1] = temp % 10; // Update the current position
-                product[i + j] += temp / 10; // Update the carry to the next position
+                int temp = x * y + product[i + j + 1]; 
+                product[i + j + 1] = temp % 10; 
+                product[i + j] += temp / 10; 
             }
         }
         
         // Convert the product array to a string
         StringBuilder sb = new StringBuilder();
         for (int digit : product) {
-            if (!(sb.length() == 0 && digit == 0)) { // Skip leading zeros
+            if (!(sb.length() == 0 && digit == 0)) {
                 sb.append(digit);
             }
         }
         
-        return sb.length() == 0 ? "0" : sb.toString(); // Handle the case when the result is "0"
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
