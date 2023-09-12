2f2211c9f0724b26c84a1bf3c018b9ccf8112ab8
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-12 23:25:38 +0300

    Time: 4 ms (46.17%), Space: 47.3 MB (72.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,31 @@
 class Solution {
+    static int[][] dir = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         boolean[][] seen = new boolean[m][n];
-        int count = 0;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
+        int ans = 0;
+        for(int i = 0 ; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(!seen[i][j] && grid[i][j] == '1'){
-                    count++;
                     seen[i][j] = true;
-                    dfs(i , j , seen , grid);
+                    dfs(seen, grid, i, j);
+                    ans++;
                 }
             }
         }
-        return count;
+        return ans;
     }
-    public void dfs(int i , int j , boolean[][] seen , char[][] g){
-        int[][] directions = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
-        for(int[] dir : directions){
-            int newI = i + dir[0];
-            int newJ = j + dir[1];
-            if(0 <= newI && newI < g.length && 0 <= newJ && newJ < g[0].length && g[newI][newJ] == '1' && !seen[newI][newJ]){
-                seen[newI][newJ] = true;
-                dfs(newI , newJ , seen , g);
+    
+    public void dfs(boolean[][] seen, char[][] grid, int i, int j){
+        for(int[] d: dir){
+            int x = i + d[0];
+            int y = j + d[1];
+            if(x >= 0 && y >= 0 && x < seen.length && y < seen[0].length && !seen[x][y] && grid[x][y] == '1'){
+                seen[x][y] = true;
+                dfs(seen, grid, x, y);
             }
         }
     }
+    
 }
