03dcfca6f81bb75fe4bb6a7c269f80dabcba88b3
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-14 04:23:10 +0300

    Time: 0 ms (100.00%), Space: 38.7 MB (95.35%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public int tribonacci(int n) {
         if(n == 0) return 0;
-        if(n == 1 || n == 2) return 1;
-        int one = 0;
-        int two = 1;
-        int three = 1;
+        else if(n == 1 || n == 2) return 1;
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
         for(int i = 3 ; i <= n ; i++){
-            int temp = three;
-            three = one + two + three;
-            one = two;
-            two = temp;
+            int t3 = t2 + t1 + t0;
+            t0 = t1;
+            t1 = t2;
+            t2 = t3;
         }
-        return three;
+        return t2;
     }
 }
