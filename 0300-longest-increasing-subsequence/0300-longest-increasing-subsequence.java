cdc177cf5aedd1f31ba24b47a4db21d37f03ed34
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-14 04:01:39 +0300

    Time: 40 ms (66.25%), Space: 43.3 MB (52.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -5,13 +5,13 @@ class Solution {
         dp[0] = 1;
         int max = 1;
         for(int i = 1 ; i < n; i++){
-            dp[i] = 1;
             for(int j = i - 1; j >= 0; j--){
                 if(nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    max = Math.max(max, dp[i]);
+                    dp[i] = Math.max(dp[i], dp[j]);
                 }
             }
+            dp[i]++;
+            max = Math.max(max, dp[i]);
         }
         return max;
     }
