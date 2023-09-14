87e305386c1043f4f9038937fd5b098ffc4d6aec
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-14 04:17:41 +0300

    Time: 0 ms (100.00%), Space: 43.6 MB (8.89%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,28 +1,13 @@
 class Solution {
-    // Tabulation
-    public int minCostClimbingStairs(int[] cost){
+    public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int two = cost[n - 1];
-        int one = cost[n - 2];
-        for(int i = n - 3 ; i >= 0 ; i--){
-            int temp = one;
-            one = Math.min(cost[i] + one , cost[i] + two);
-            two = temp;
+        int a = cost[n - 1];
+        int b = cost[n - 2];
+        for(int i = n - 3; i >= 0 ; i--){
+            int temp = b;
+            b = Math.min(a , b) + cost[i];
+            a = temp;
         }
-        return Math.min(one , two);
+        return Math.min(a,b);
     }
-//     Top down
-    // public int minCostClimbingStairs(int[] cost) {
-    //     int n = cost.length;
-    //     int[] memo = new int[n];
-    //     dp(0 , cost , memo);
-    //     return Math.min(memo[0] , memo[1]);
-    // }
-    // public int dp(int n , int[] cost , int[] memo){
-    //     if(n >= cost.length) return 0;
-    //     if(memo[n] != 0) return memo[n];
-    //     int c = cost[n] + Math.min(dp(n + 1 , cost , memo) , dp(n + 2 , cost , memo));
-    //     memo[n] = c;
-    //     return c;
-    // }
 }
