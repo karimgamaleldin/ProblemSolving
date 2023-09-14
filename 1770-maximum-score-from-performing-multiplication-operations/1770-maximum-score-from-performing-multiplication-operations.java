f7a434739b89f0d6052d24bfd7692472d3e51b45
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-14 17:13:43 +0300

    Time: 251 ms (5.75%), Space: 58.8 MB (23.45%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,39 +1,25 @@
 class Solution {
-    public int maximumScore(int[] nums , int[] multipliers){
-        int n = nums.length;
-        int m = multipliers.length;
-        int[][] tab = new int[m + 1][m + 1];
-        for(int[] me : tab) Arrays.fill(me , 0);
-        for(int i = m - 1 ; i >= 0 ; i--){
-            for(int j = i; j >= 0 ; j--){
-                int start = multipliers[i] * nums[j] + tab[i + 1][j + 1]; 
-                int end   = multipliers[i] * nums[n - 1 - (i - j)] + tab[i + 1][j]; 
-                tab[i][j] = Math.max(start , end);
-            }
-        }
-        return tab[0][0];
+    HashMap<String, Integer> map;
+    int[] nums;
+    int[] muls;
+    public int maximumScore(int[] nums, int[] multipliers) {
+        this.map = new HashMap<>();
+        this.nums = nums;
+        this.muls = multipliers;
+        int k = dp(0, nums.length - 1, 0);
+        return k;
     }
-    // // Bottom UP
-    // HashMap<String , Integer> memo = new HashMap<>();
-    // int[] nums;
-    // int[] multipliers;
-    // public int maximumScore(int[] nums, int[] multipliers) {
-    //     int n = nums.length;
-    //     int m = multipliers.length;
-    //     this.nums = nums;
-    //     this.multipliers = multipliers;
-    //     return dp(0 , n - 1 , 0);
-    // }
-    // public int dp(int i , int j , int m){
-    //     if(i > j || m == multipliers.length) return 0;
-    //     else if(memo.containsKey(keyMap(i , j , m))) return memo.get(keyMap(i , j , m));
-    //     int start = nums[i] * multipliers[m] + dp(i + 1 , j , m + 1);
-    //     int end   = nums[j] * multipliers[m] + dp(i , j - 1 , m + 1);
-    //     int x = Math.max(start , end);
-    //     memo.put(keyMap(i , j , m) , x);
-    //     return x;
-    // }
-    // public String keyMap(int i , int j , int m){
-    //     return "" + i + " " + j + " " + m;
-    // }
+    
+    public int dp(int i, int j, int len){
+        if(i > j || len == muls.length) return 0;
+        String s = getKey(i, j, len);
+        if(map.containsKey(s)) return map.get(s);
+        int start = nums[i] * muls[len] + dp(i + 1 , j , len + 1);
+        int end = nums[j] * muls[len] + dp(i, j - 1, len + 1);
+        map.put(s, Math.max(start, end));
+        return Math.max(start, end);
+    }
+    public String getKey(int i, int j, int k){
+        return "" + i + "," + j + "," + k;
+    }
 }
