7a0676f6ec2caa403df99e4a1c76dfd6bbe491d9
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-14 03:17:01 +0300

    Time: 0 ms (100.00%), Space: 39.9 MB (52.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,31 +1,15 @@
 class Solution {
-    public int rob(int[] nums){
+    public int rob(int[] nums) {
+        if(nums.length == 0) return 0;
+        else if (nums.length == 1) return nums[0];
         int n = nums.length;
-        if(n == 1) return nums[0];
-        int two = nums[0];
-        int one = Math.max(nums[0] , nums[1]);
+        int o = nums[0];
+        int t = Math.max(nums[0], nums[1]);
         for(int i = 2 ; i < n ; i++){
-            int temp = one;
-            one = Math.max(two + nums[i] , one);
-            two = temp;
+            int temp = t;
+            t = Math.max(o + nums[i] , t);
+            o = temp;
         }
-        return Math.max(one , two);
+        return Math.max(o,t);
     }
-    // // Top down implementation
-    // private HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
-    // private int[] nums;
-    // public int rob(int[] nums) {
-    //     int n = nums.length;
-    //     this.nums = nums;
-    //     if(n == 1) return dp(n-1);
-    //     return dp(n - 1); // As we can stop in the before last house
-    // }
-    // public int dp(int n){
-    //     if(n == 0) return nums[0];
-    //     if(n == 1) return Math.max(nums[1] , nums[0]);
-    //     if(memo.containsKey(n)) return memo.get(n);
-    //     int x = Math.max(dp(n-2) +  nums[n] , dp(n-1));
-    //     memo.put(n , x);
-    //     return x;
-    // }
 }
