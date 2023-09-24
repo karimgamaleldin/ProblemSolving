04798cf718a82d9e7aec515f49f4dccd5372271b
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-09-24 10:13:13 +0300

    Time: 0 ms (100.00%), Space: 50.5 MB (13.35%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,19 +1,16 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int min = 0;
-        int max = nums.length - 1;
-        while(min < max){
-            int mid = (max + min) / 2;
-            if(mid % 2 == 1){ // as we are always using the even index as its duplicate will be on the side
-                mid--;
-            }
-            if( nums[mid] == nums[mid + 1]){ // comparing the even index with it's odd, if == then the subarray from min to max is ok
-                min = mid + 2; // as we made a --
-            } else{ // otherwise we will find the number on the left as the sequence will be broken
-                max = mid; // as we already made a --
-            }
+        int l = 0;
+        int r = nums.length - 1;
+        if(nums.length == 1 || nums[l] != nums[l + 1]) return nums[l];
+        if(nums[r] != nums[r - 1]) return nums[r];
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) return nums[mid];
+            else if(nums[mid] == nums[mid + 1] && mid % 2 != 0 || (nums[mid] == nums[mid - 1] && mid % 2 != 1)) r = mid - 1;
+            else l = mid + 1;
+            // System.out.println(mid + "-" + nums[mid]);
         }
-        return nums[min];
-        
+        return -1;
     }
 }
