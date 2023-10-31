2d9a559b931019fe7f3373811d5ab4d3da68feab
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-10-31 22:42:29 +0200

    Time: 2 ms (81.64%), Space: 43.4 MB (78.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0 ; i < nums.length; i++){
             int n = nums[i];
             int x = target - n;
-            if(map.containsKey(x)) return new int[]{map.get(x), i};
+            if(map.containsKey(x)) return new int[] {map.get(x) , i};
             map.put(n, i);
         }
         return null;
