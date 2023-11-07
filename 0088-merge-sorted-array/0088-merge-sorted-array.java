2355ad1a1bd4dccc4097ede57450120d6ee5d3fd
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-07 20:29:36 +0200

    Time: 0 ms (100.00%), Space: 40.9 MB (92.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,24 +1,16 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = nums1.length - 1;
-        while(i >= 0 || j >= 0){
+        int oi = m - 1;
+        int ti = n - 1;
+        int i = m + n - 1;
+        while(oi >= 0 || ti >= 0){
             int x = Integer.MIN_VALUE;
-            int f = 0;
-            if(i >= 0){
-                x = Math.max(x , nums1[i]);
-                f = 1;
-            }
-            if(j >= 0) {
-                if(x < nums2[j]){
-                    f = 2;
-                    x = nums2[j];
-                }
-            }
-            if(f == 1) i--;
-            else j--;
-            nums1[k--] = x;
+            int y = x;
+            if(oi >= 0) x = nums1[oi];
+            if(ti >= 0) y = nums2[ti];
+            nums1[i--] = Math.max(x, y);
+            if(x > y) oi--;
+            else ti--;
         }
     }
 }
