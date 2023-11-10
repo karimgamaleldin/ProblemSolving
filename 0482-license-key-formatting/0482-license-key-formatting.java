32d12001b1087ada04b1f5760460cfa11cd346eb
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-10 13:16:12 +0200

    Time: 140 ms (18.83%), Space: 43.9 MB (31.05%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.java
+++ 0482-license-key-formatting/0482-license-key-formatting.java
@@ -1,25 +1,19 @@
 class Solution {
     public String licenseKeyFormatting(String s, int k) {
+        s = s.toUpperCase();
+        int count = 0;
         StringBuilder sb = new StringBuilder();
         int n = s.length() - 1;
-        int count = 0;
-        for(int i = n; i >= 0 ; i--){
+        for(int i = n; i >= 0; i--){
             char c = s.charAt(i);
-            if(c != '-'){
-                sb.append(capitalize(c));
-                count++;
-            }
+            if(c == '-') continue;
             if(count == k){
-                if(i != 0) sb.append('-');
                 count = 0;
+                sb.insert(0, '-');
             }
+            sb.insert(0, c);
+            count++;
         }
-        StringBuilder res = sb.reverse();
-        while(res.length() > 0 && res.charAt(0) == '-') res.deleteCharAt(0);
-        return res.toString();
-    }
-    public char capitalize(char c){
-        if(c < 'a') return c;
-        return (char) (c - 32);
+        return sb.toString();
     }
 }
