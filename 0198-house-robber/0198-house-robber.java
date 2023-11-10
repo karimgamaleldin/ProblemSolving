b2e2120dba2d1829306f9b7e184b37b8107cd77b
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-10 11:21:54 +0200

    Time: 0 ms (100.00%), Space: 39.9 MB (30.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        else if (nums.length == 1) return nums[0];
-        int n = nums.length;
-        int o = nums[0];
-        int t = Math.max(nums[0], nums[1]);
-        for(int i = 2 ; i < n ; i++){
-            int temp = t;
-            t = Math.max(o + nums[i] , t);
-            o = temp;
+        if(nums.length == 1) return nums[0];
+        int one = Math.max(nums[1], nums[0]);
+        int two = nums[0];
+        for(int i = 2; i < nums.length; i++){
+            int temp = one;
+            one = Math.max(one, two + nums[i]);
+            two = temp;
+            // System.out.println("one " + one);
+            // System.out.println(two);
         }
-        return t;
+        return Math.max(one, two);
     }
 }
