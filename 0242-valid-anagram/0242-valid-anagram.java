174fee1cf7549e0f3e75d37cb1bed689a58c6e1c
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-10 09:40:40 +0200

    Time: 4 ms (74.50%), Space: 42 MB (77.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        // bucket sort
-        int n = t.length();
-        int[] alph = new int[26];
-        for(int i = 0 ; i < n; i++){
-            alph[t.charAt(i) - 'a']++;
-            alph[s.charAt(i) - 'a']--;
+        int n = s.length();
+        if(n != t.length()) return false;
+        int[] alph1 = new int[26];
+        int[] alph2 = new int[26];
+        for(int i = 0 ; i < n ; i++){
+            alph1[s.charAt(i) - 'a']++;
+            alph2[t.charAt(i) - 'a']++;
         }
-        for(int x : alph) if(x != 0) return false;
+        for(int i = 0 ; i < 26 ; i++){
+            if(alph1[i] != alph2[i]) return false;
+        }
         return true;
     }
 }
