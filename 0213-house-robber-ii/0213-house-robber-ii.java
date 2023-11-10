3c3772a31421a2582015d8ac76828f41156f88a9
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-10 11:27:02 +0200

    Time: 0 ms (100.00%), Space: 40.2 MB (15.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,20 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        else if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        // start from the 0 to n - 2 and from 1 to n - 1
         int n = nums.length;
-        int o = nums[0];
-        int t = Math.max(nums[0] , nums[1]);
-        int th = nums[1];
-        int f = Math.max(nums[1], nums[2]);
-        for(int i = 2 ; i < n - 1; i++){
-            int temp = t;
-            t = Math.max(t , o + nums[i]);
-            o = temp;
-            temp = f;
-            f = Math.max(f, th + nums[i + 1]);
-            th = temp;
+        if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0] , nums[1]);
+        int one = Math.max(nums[0], nums[1]);
+        int two = nums[0];
+        for(int i = 2 ; i < n - 1 ; i++){
+            int temp = one;
+            one = Math.max(one , two + nums[i]);
+            two = temp;
         }
-        return Math.max(f, t);
+        int one2 = Math.max(nums[1], nums[2]);
+        int two2 = nums[1];
+        for(int i = 3; i < n ; i++){
+            int temp = one2;
+            one2 = Math.max(one2, two2 + nums[i]);
+            two2 = temp;
+        }
+        return Math.max(one2, one);
     }
 }
