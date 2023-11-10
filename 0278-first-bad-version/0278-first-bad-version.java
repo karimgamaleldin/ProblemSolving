b558d152c6103445d49544f96014e15c5481cf22
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-10 10:25:53 +0200

    Time: 13 ms (81.38%), Space: 38.9 MB (66.34%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,16 +3,19 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int ans = 0;
-        int l = 0;
-        int r = n;
-        while(l <= r){
-            int mid = l + (r - l) / 2;
+        int i = 1;
+        int j = n;
+        int first = -1;
+        while(i <= j){
+            int mid = i + ( j - i ) / 2;
             if(isBadVersion(mid)){
-                ans = mid;
-                r = mid - 1;
-            } else l = mid + 1;
+                first = mid;
+                j = mid - 1;
+            }
+            else{
+                i = mid + 1;
+            }
         }
-        return ans;
+        return first;
     }
 }
