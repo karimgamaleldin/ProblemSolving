21417b6180d065602a1b239830743008b6934cd3
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-11 09:58:00 +0200

    Time: 2 ms (83.09%), Space: 40.7 MB (15.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,15 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i = 0 ; i < s.length() ; i++){
+        for(int i = 0; i < s.length() ; i++){
             char c = s.charAt(i);
-            if(c == '[' || c == '(' || c == '{') stack.push(c);
-            else if(stack.isEmpty()) return false;
-            else if(c == ']' && stack.peek() == '[' || c == ')' && stack.peek() == '(' || stack.peek() == '{' && c == '}'){
-                stack.pop();
+            if(checkOpenBracket(c)) stack.push(c);
+            else{
+                if(stack.isEmpty() || !checkSimilarBrackets(stack.pop(), c)) return false;
             }
-            else return false;
+            
         }
         return stack.isEmpty();
+    }
+    
+    private boolean checkOpenBracket(char c){
+        return c == '(' || c == '[' || c == '{';
+    }
+    
+    private boolean checkSimilarBrackets(char c, char d){
+        if(d == ')') return c == '(';
+        else if (d == ']') return c == '[';
+        return c == '{';
     }
 }
