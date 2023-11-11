ea1b84148845c25d79ba4eb5c206b01341763a8a
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-11 10:16:09 +0200

    Time: 57 ms (17.34%), Space: 46.1 MB (96.78%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,21 +1,23 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            double x = euc(a); double y = euc(b);
-            if(x < y) return 1;
-            return -1;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> {
+            double x = eucledianDistanceToOrigin(a);
+            double y = eucledianDistanceToOrigin(b);
+            if(x > y) return -1;
+            else if(x < y) return 1;
+            return 0;   
         });
-        for(int[] point : points){
-            pq.add(point);
-            if(pq.size() > k) pq.remove();
+        
+        for(int[] p: points){
+            pq.add(p);
+            if(pq.size() > k) pq.poll();
         }
-        int[][] ans = new int[k][];
-        for(int i = 0 ; i < k ; i++){
-            ans[i] = pq.remove();
-        }
-        return ans;
+        int[][] sol = new int[k][2];
+        for(int i = k - 1; i >= 0; i--) sol[i] = pq.poll();
+        return sol;
     }
-    public double euc(int[] x){
-        return  Math.sqrt(Math.pow(x[0], 2) + Math.pow(x[1], 2));
+    
+    private double eucledianDistanceToOrigin(int[] point1){
+        return Math.sqrt(Math.pow(point1[0], 2) + Math.pow(point1[1], 2));
     }
 }
