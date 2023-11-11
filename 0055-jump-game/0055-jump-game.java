debd261b699e2f84f5fda86c0dd82c6c0fce2ff3
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-11 06:24:02 +0200

    Time: 1 ms (99.99%), Space: 44.5 MB (28.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int goal = nums.length - 1;
-        int i = nums.length - 1;
+        int n = nums.length - 1;
+        int goal = n;
+        int i = n - 1;
         while(i >= 0){
-            if(nums[i] + i >= goal)
+            if(nums[i] + i >= goal) {
                 goal = i;
+            }
             i--;
         }
         return goal == 0;
