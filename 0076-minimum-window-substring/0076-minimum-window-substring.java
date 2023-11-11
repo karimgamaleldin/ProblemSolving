aa7f5832be653de4e229165b42c3db592634de13
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-11 06:56:37 +0200

    Time: 70 ms (17.90%), Space: 44.4 MB (32.17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,37 +1,29 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int m = s.length() , n = t.length();
-        Map<Character , Integer> map1 = new HashMap<>();
-        Map<Character , Integer> map2 = new HashMap<>();
-        for(int i = 0 ; i < n ; i++) map2.put(t.charAt(i) , map2.getOrDefault(t.charAt(i) , 0) + 1);
-        int l = 0;
-        int r = 0;
-        String ans = "";
-        int min = Integer.MAX_VALUE;
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+        for(int i = 0; i < t.length() ; i++) countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        String res = null;
         int count = 0;
-        while(r < m){
-            char c = s.charAt(r);
-            int x = map1.getOrDefault(c , 0) + 1;
-            int y = map2.getOrDefault(c, -1);
-            map1.put(c, x);
-            if(x <= y) count++;
-            while(count == n){
-                if(r - l + 1 < min){
-                    min = r - l + 1;
-                    ans = s.substring(l, r + 1);
-                }
-                char d = s.charAt(l);
-                if(map2.containsKey(d) && map1.get(d) <= map2.get(d)) count--;
-                map1.put(d, map1.get(d) - 1);
-                l++;
+        int left = 0;
+        for(int i = 0; i < s.length() ; i++){
+            char c = s.charAt(i);
+            countS.put(c, countS.getOrDefault(c, 0) + 1);
+            if(countT.getOrDefault(c, Integer.MIN_VALUE) >= countS.get(c)) count++;
+            while(count == t.length()){
+                char d = s.charAt(left);
+                int x = res == null ? Integer.MAX_VALUE : res.length();
+                if(i - left + 1 < x)res = s.substring(left , i + 1);
+                countS.put(d, countS.get(d) - 1);
+                if(countT.getOrDefault(d, Integer.MIN_VALUE) > countS.get(d)) count--;
+                if(countS.get(d) == 0) countS.remove(d);
+                left++;
+                // System.out.println(res);
             }
-            r++;
         }
-        // System.out.println(map1);
-        // System.out.println(map1.get('a'));
-        // System.out.println(map2.get('a'));
-        // System.out.println(map2);
-        // System.out.println(counts +" "+ distinct);
-        return ans;
+        // System.out.println(countT);
+        // System.out.println(countS);
+        // System.out.println(count);
+        return res == null ? "" : res;
     }
 }
