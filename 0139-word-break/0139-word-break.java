476f5aa95f13d0d1295c51573dd31586df330a0f
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-12 14:20:58 +0200

    Time: 8 ms (34.98%), Space: 44 MB (7.52%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,30 +1,19 @@
 class Solution {
-    HashSet<String> dict;
-    String s;
-    int n;
-    HashMap<Integer , Boolean> memo = new HashMap<>();
     public boolean wordBreak(String s, List<String> wordDict) {
-        this.dict = new HashSet<>(wordDict);
-        this.s = s;
-        this.n = s.length();
-        return dp(0);
-    }
-    public boolean dp(int i){
-        if(i == n) return true;
-        if(memo.containsKey(i)) return memo.get(i);
-        boolean f = false;
-        for(int j = i ; j < n ; j++){
-            String t = s.substring(i , j + 1);
-            if(dict.contains(t)){
-                boolean f2 = dp(j + 1);
-                if(f2){
-                    f = true;
+        int n = s.length();
+        HashSet<String> dict = new HashSet<>(wordDict);
+        boolean dp[] = new boolean[n + 1];
+        dp[n] = true;
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = i; j < n; j++){
+                String s2 = s.substring(i, j + 1);
+                if(dict.contains(s2) && dp[j + 1]){
+                    dp[i] = true;
                     break;
                 }
             }
         }
-        memo.put(i , f);
-        return f;
+        return dp[0];
     }
     
 }
