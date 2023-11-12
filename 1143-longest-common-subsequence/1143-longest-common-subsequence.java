a942f868d6290d5f4d7f4e8bb9aeb6d4e2d14b99
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-12 13:19:56 +0200

    Time: 20 ms (82.68%), Space: 48.1 MB (61.99%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,40 +1,16 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int[][] tab = new int[n + 1][m + 1];
-        for(int i = n - 1 ; i >= 0 ;i--){
-            for(int j = m - 1 ; j >= 0 ; j--){
-                char c = text1.charAt(i) , d = text2.charAt(j);
-                if(c == d) tab[i][j] = 1 + tab[i + 1][j + 1];
-                else tab[i][j] = Math.max(tab[i + 1][j] , tab[i][j + 1]);
+        int n = text1.length() + 1;
+        int m = text2.length() + 1;
+        int[][] dp = new int[n][m];
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = m - 2; j >= 0 ; j--){
+                char a = text1.charAt(i);
+                char b = text2.charAt(j);
+                if(a == b) dp[i][j] = 1 + dp[i + 1][j + 1];
+                else dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
             }
         }
-        return tab[0][0];
+        return dp[0][0];
     }
-    // HashMap<String , Integer> memo = new HashMap<>();
-    // String s1;
-    // String s2;
-    // public int longestCommonSubsequence(String text1, String text2) {
-    //     this.s1 = text1;
-    //     this.s2 = text2;
-    //     return dp(0 , 0);
-    // }
-    // public int dp(int i , int j){
-    //     if(i >= s1.length() || j >= s2.length()) return 0;
-    //     else if(memo.containsKey(keyGetter(i , j))) return memo.get(keyGetter(i , j));
-    //     char c = s1.charAt(i);
-    //     char d = s2.charAt(j);
-    //     int t = 0;
-    //     if(c == d){
-    //         t = dp(i + 1 , j + 1) + 1;
-    //     }
-    //     else t = Math.max(dp(i + 1 , j) , Math.max(dp(i + 1 , j + 1) , dp(i , j + 1)));
-    //     memo.put(keyGetter(i , j) , t);
-    //     return t;
-    // }
-    // public String keyGetter(int i , int j){
-    //     return i + " " + j;
-    // }
-    
 }
