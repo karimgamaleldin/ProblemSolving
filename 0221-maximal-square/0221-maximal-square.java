0013d9fa2fc8c7730f5455a9c10d3527be105a88
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-12 13:47:28 +0200

    Time: 11 ms (17.99%), Space: 53.8 MB (66.27%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m+1][n+1];
+        int max = 0;
+        for(int i = m -1 ; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(matrix[i][j] == '1'){
+                    int x = Math.min(dp[i+1][j] , Math.min(dp[i+1][j+1] , dp[i][j+1]));
+                    x = (int)Math.sqrt(x) + 1;
+                    dp[i][j] = Math.max(1, x * x);
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max;
+    }
+    boolean checkOne(char c){
+        return c == '1';
+    }
+}
