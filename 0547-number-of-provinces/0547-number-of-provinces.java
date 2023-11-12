8270f94793d7923e5a8a18744bb98d9244582e6f
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-12 20:43:56 +0200

    Time: 2 ms (50.38%), Space: 46.8 MB (5.53%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,49 +1,49 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        UnionFindOptimized uf = new UnionFindOptimized(n);
-        for(int i = 0 ; i < n ; i++){
-            int[] arr = isConnected[i];
-            for(int j = 0 ; j < n ; j++){
-                if(i != j && arr[j] == 1)uf.union(i , j);
+        UnionFind uf = new UnionFind(isConnected.length);
+        int j = 0;
+        for(int[] arr: isConnected){
+            for(int i = 0 ; i < arr.length; i++){
+                if(arr[i] == 1) uf.union(i, j);
             }
+            
+            j++;
         }
-        return uf.numberOfComponents();
+        return uf.count;
     }
-    static class UnionFindOptimized{
-        private int[] root;
-        private int[] rank;
-        public UnionFindOptimized(int size){ // Initialize the root and rank array
-            this.root = new int[size];
-            this.rank = new int[size];
-            for(int i = 0 ; i < size ; i++){
+    
+    static class UnionFind{
+        int[] root;
+        int[] rank;
+        int count;
+        public UnionFind(int n){
+            this.root = new int[n];
+            this.rank = new int[n];
+            for(int i = 0; i < n; i++){
                 this.root[i] = i;
                 this.rank[i] = 1;
             }
+            count  = n;
         }
-
+        
         public int find(int x){
-            if(root[x] == x) return x;
+            if(x == root[x]) return x;
             return root[x] = find(root[x]);
         }
-
+        
         public void union(int x , int y){
-            int rootX = find(x);
-            int rootY = find(y);
-            if(rank[rootX] < rank[rootY]) root[rootX] = rootY;
-            else if(rank[rootX] > rank[rootY]) root[rootY] = rootX;
+            int a = find(x);
+            int b = find(y);
+            if(a == b) return;
+            if(rank[a] > rank[b]) root[b] = a;
+            else if(rank[a] < rank[b]) root[a] = b;
             else{
-                root[rootY] = rootX;
-                rank[rootX]++;
+                root[b] = a;
+                rank[b] = 1;
             }
+            count--;
         }
         
-        public boolean connected(int x , int y){return find(x) == find(y);}
         
-        public int numberOfComponents(){
-            HashSet<Integer> s = new HashSet<>();
-            for(int i = 0 ; i < root.length ; i++) s.add(find(root[i]));
-            return s.size();
-        }
     }
 }
