83c6d40fffc54bda3cd2abe53d391e692f2e41a0
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-12 22:18:38 +0200

    Time: 93 ms (61.07%), Space: 150 MB (47.04%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,26 +1,28 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            map.put(i, new ArrayList<>());
-        }
-        
         for(int[] e: edges){
+            if(!map.containsKey(e[0])) map.put(e[0], new ArrayList<>());
+            if(!map.containsKey(e[1])) map.put(e[1], new ArrayList<>());
             map.get(e[0]).add(e[1]);
             map.get(e[1]).add(e[0]);
         }
+        Queue<Integer> q = new LinkedList<>();
         HashSet<Integer> seen = new HashSet<>();
+        q.add(source);
         seen.add(source);
-        dfs(map, seen, source);
-        return seen.contains(destination);
-    }
-    
-    public void dfs(HashMap<Integer, ArrayList<Integer>> map,HashSet<Integer> seen, int curr){
-        ArrayList<Integer> arr = map.get(curr);
-        for(int x: arr){
-            if(seen.contains(x)) continue;
-            seen.add(x);
-            dfs(map, seen, x);
+        while(!q.isEmpty()){
+            int node = q.remove();
+            List<Integer> l = map.get(node);
+            if(l == null) continue;
+            for(int a : l){
+                if(a == destination) return true;
+                if(!seen.contains(a)){
+                    q.add(a);
+                    seen.add(a);
+                }
+            }
         }
+        return source == destination;
     }
 }
