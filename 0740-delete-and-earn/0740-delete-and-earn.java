aa6dfdb166041cd12f42869f257b990f9239c5d6
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-12 12:26:35 +0200

    Time: 11 ms (49.30%), Space: 43 MB (78.03%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,46 +1,23 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        HashMap<Integer , Integer> countMap = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-        int[] tab = new int[n];
-        Arrays.sort(nums);
-        for(int num : nums){
-            countMap.put(num , countMap.getOrDefault(num , 0) + 1);
+        int max = nums[0];
+        int min = nums[0];
+        for(int i = 0; i < n ; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            max= Math.max(max, nums[i]);
+            min= Math.min(min, nums[i]);
         }
-        for(int i = n - 1 ; i >= 0 ; i--){
-            if(i != 0 && nums[i-1] == nums[i]) continue;
-            int t = countMap.get(nums[i]);
-            int u = countMap.containsKey(nums[i] + 1) ? countMap.get(nums[i] + 1) : 0 ;
-            int x = t * nums[i];
-            if(t + i + u < n){
-                x += tab[t + i + u];
-            }
-            if(u != 0){
-                x = Math.max(x , tab[i + t]);
-            }
-            tab[i] = x;
+        int two = map.getOrDefault(min, 0) * min;
+        int one = Math.max(map.getOrDefault(min + 1, 0) * (min + 1), two);
+        for(int i = min + 2 ; i <= max; i++){
+            // System.out.println(one + "," + two);
+            int temp = one;
+            one = Math.max(one, two + map.getOrDefault(i, 0) * i);
+            two = temp;
+            // System.out.println(one + "," + two);
         }
-        return tab[0];
+        return Math.max(one, two);
     }
-    // Memoization
-//     public int deleteAndEarn(int[] nums) {
-//         HashMap<Integer , Integer> countMap = new HashMap<>();
-//         HashMap<Integer , Integer> memo = new HashMap<>();
-//         Arrays.sort(nums);
-//         for(int num : nums){
-//             countMap.put(num , countMap.getOrDefault(num , 0) + 1);
-//         }
-//         return dp(0 , nums , countMap , memo);
-//     }
-    
-//     public int dp(int i , int[] nums , HashMap<Integer , Integer> counts , HashMap<Integer , Integer> memo){
-//         if(i >= nums.length) return 0;
-//         else if(memo.containsKey(nums[i])) return memo.get(nums[i]);
-//         int x = counts.get(nums[i]);
-//         int y = counts.containsKey(nums[i] + 1) ? counts.get(nums[i] + 1) : 0;
-//         int take = nums[i] * x + dp(i + x + y , nums , counts , memo);
-//         int skip = dp(i + x , nums , counts , memo);
-//         memo.put(nums[i] , Math.max(take , skip));
-//         return memo.get(nums[i]);
-//     }
 }
