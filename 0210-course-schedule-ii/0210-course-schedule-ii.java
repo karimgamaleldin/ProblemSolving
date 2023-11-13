d75e2447fa283680d15fd57b5cb236808a5d8d2e
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-13 23:14:07 +0200

    Time: 6 ms (59.58%), Space: 45.1 MB (8.11%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,33 +1,27 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] indeg = new int[numCourses];
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        for(int i = 0 ; i < numCourses; i++) map.put(i, new ArrayList<>());
-        for(int[] p: prerequisites){
-            indeg[p[0]]++;
-            map.get(p[1]).add(p[0]);
+        HashMap<Integer, List<Integer>> adgLists = new HashMap<>();
+        for(int i = 0; i < numCourses; i++) adgLists.put(i, new ArrayList<>());
+        for(int[] pre : prerequisites){
+            indeg[pre[0]]++;
+            adgLists.get(pre[1]).add(pre[0]);
         }
-        List<Integer> arr = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < numCourses; i++){
-            if(indeg[i] == 0) {
-                q.add(i);
-            }
-        }
+        for(int i = 0 ; i < numCourses; i++) if(indeg[i] == 0) q.add(i);
+        int i = 0;
+        int[] sol = new int[numCourses];
         while(!q.isEmpty()){
-            int cour = q.remove();
-            arr.add(cour);
-            List<Integer> li = map.get(cour);
-            for(int x: li){
-                indeg[x]--;
-                if(indeg[x] == 0) q.add(x);
+            int x = q.remove();
+            List<Integer> l = adgLists.get(x);
+            for(int n : l){
+                indeg[n]--;
+                if(indeg[n] == 0){
+                    q.add(n);
+                }
             }
+            sol[i++] = x;
         }
-        if(arr.size() != numCourses) return new int[0];
-        int[] ans = new int[numCourses];
-        for(int i = 0 ; i < numCourses ; i++){
-            ans[i] = arr.get(i);
-        }
-        return ans;
+        return i == numCourses ? sol : new int[]{};
     }
 }
