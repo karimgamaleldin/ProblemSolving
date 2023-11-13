3421046250be8cccc756d10ae2f25dad767b390d
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-13 10:20:10 +0200

    Time: 0 ms (100.00%), Space: 40.4 MB (57.44%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,17 +3,13 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         if(obstacleGrid[m - 1][n - 1] == 1) return 0;
-        int[][] tab = new int[m][n];
-        tab[m - 1][n - 1] = 1;
-        for(int i = m - 1; i >= 0 ; i--){
-            for(int j = n - 1 ; j >= 0 ; j--){
-                if(obstacleGrid[i][j] == 1) continue;
-                int x = 0;
-                if(i != m - 1) x += tab[i + 1][j];
-                if(j != n - 1) x += tab[i][j + 1];
-                tab[i][j] += x;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[m - 1][n] = 1;
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(obstacleGrid[i][j] != 1) dp[i][j] = dp[i+1][j] + dp[i][j+1];
             }
         }
-        return tab[0][0];
+        return dp[0][0];
     }
 }
