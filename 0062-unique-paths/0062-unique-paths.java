43fc5eaa746ace232f11068c891237a100dbc053
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-11-13 10:15:16 +0200

    Time: 0 ms (100.00%), Space: 38.9 MB (84.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,15 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] tab = new int[m][n];
-        tab[m - 1][n - 1] = 1;
-        for(int i = m - 1 ; i >= 0 ; i--){
-            for(int j = n - 1 ; j >= 0 ; j--){
-                int x = 0;
-                if(i + 1 != m) x += tab[i + 1][j];
-                if(j + 1 != n) x += tab[i][j + 1];
-                tab[i][j] += x;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[m - 1][n] = 1;
+        for(int i = m - 1; i >= 0 ; i--){
+            for(int j = n - 1; j >= 0 ; j--){
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1]; 
             }
         }
-        return tab[0][0];
+        return dp[0][0];
     }
 }
