3f79be466acd1760cbe9853567739093573f390d
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-12-06 09:42:28 +0200

    Time: 6 ms (13.54%), Space: 46.8 MB (85.70%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -7,20 +7,18 @@ class MinStack {
     }
     
     public void push(int val) {
-        int min;
-        if(this.stack.empty()) min = val; else min = Math.min(minStack.peek(),val);
+        int min = stack.isEmpty() ? val : Math.min(val, minStack.peek());
         stack.push(val);
-        minStack.push(min);   
+        minStack.push(min);
     }
     
     public void pop() {
-        minStack.pop();
         stack.pop();
+        minStack.pop();
     }
     
     public int top() {
         return stack.peek();
-        
     }
     
     public int getMin() {
