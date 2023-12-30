9a3ae37fe63300ffcb9b20187f7a6367a45edd4f
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2023-12-30 08:24:34 +0200

    Time: 241 ms (5.06%), Space: 46.3 MB (22.70%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,19 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        for(int i = 0 ; i < n + 1 ; i++) dp[i][0] = 1;
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1; j <= amount ; j++){
-                int x = j - coins[i - 1] < 0 ? 0 : dp[i][j - coins[i - 1]];
-                dp[i][j] = x + dp[i - 1][j];
-            }
-        }
-        // printMatrix(dp);
-        return dp[n][amount];
+        int[][] memo = new int[amount + 1][coins.length];
+        for(int[] me : memo) Arrays.fill(me, -1);
+        return dp(amount, 0, memo, coins);
     }
-    void printMatrix(int[][] x){
-        for(int i = 0 ; i < x.length; i++){
-            for(int j = 0; j < x[0].length; j++){
-                System.out.print(x[i][j] + ", ");
-            }
-            System.out.println();
+    
+    public int dp(int amount, int index, int[][] memo, int[] coins){
+        if(index >= coins.length || amount < 0) return 0;
+        if(amount == 0) return 1;
+        if(memo[amount][index] != -1) return memo[amount][index];
+        int total = 0;
+        for(int i = index; i < coins.length ; i++){
+            total += dp(amount - coins[i], i, memo, coins);   
         }
+        memo[amount][index] = total;
+        return total;
     }
 }
