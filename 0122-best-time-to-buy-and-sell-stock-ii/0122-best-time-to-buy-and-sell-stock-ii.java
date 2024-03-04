326daf423f76ff5ffe04e463fc3dd3c06a6f1b06
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2024-03-04 19:15:08 +0200

    Time: 4 ms (7.06%), Space: 49.1 MB (5.02%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[][] memo = new int[n][2];
-        for(int[] me : memo) Arrays.fill(me, -1);
-        return dp(prices, memo, 0, 0);
+        int[][] memo = new int[prices.length][2];
+        for (int[] m: memo) Arrays.fill(m, -1);
+        return dp(prices, 0, 0, memo);
     }
     
-    public int dp(int[] prices, int[][] memo, int i, int j){
+    public int dp(int[] prices, int i, int hold, int[][] memo){
         if(i == prices.length) return 0;
-        if(memo[i][j] != -1) return memo[i][j];
-        if(j == 0){
-            memo[i][j] = Math.max(dp(prices, memo, i + 1, j), dp(prices, memo, i + 1, 1) - prices[i]);
+        if (memo[i][hold] != -1) return memo[i][hold];
+        if (hold == 0){
+            memo[i][hold] = Math.max(dp(prices, i+1, 1, memo) - prices[i], dp(prices, i+1, 0, memo));
         }else{
-            memo[i][j] = Math.max(dp(prices, memo, i + 1, j), dp(prices, memo, i + 1, 0) + prices[i]);
+            memo[i][hold] = Math.max(dp(prices, i+1, 0, memo) + prices[i], dp(prices, i+1, 1, memo));
         }
-        return memo[i][j];
+        return memo[i][hold];
     }
 }
