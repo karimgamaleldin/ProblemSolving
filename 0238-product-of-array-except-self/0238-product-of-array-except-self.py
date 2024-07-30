b9de179cd71dd7444269ff41bbb8b49cbbd99ec2
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2024-07-30 17:13:06 +0300

    Time: 249 ms (98.00%), Space: 25.7 MB (47.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        ans = [0] * n
-        prefixes = [1] * n
-        suffixes = [1] * n
-        for i in range(n - 1):
-            prefixes[i + 1] *= prefixes[i] * nums[i]
-            suffixes[n - 2 - i] *= suffixes[n - 1 - i] * nums[n - 1 - i]
-        ans[0], ans[n - 1] = suffixes[0], prefixes[n - 1]
-        for i in range(1, n - 1):
-            ans[i] = prefixes[i] * suffixes[i]
-        return ans
+        res = [1] * len(nums)
+        for i in range(1, n):
+            res[i] = res[i - 1] * nums[i - 1]
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            res[i] = suffix * res[i]
+            suffix *= nums[i]
+        return res
+        # n = len(nums)
+        # ans = [0] * n
+        # prefixes = [1] * n
+        # suffixes = [1] * n
+        # for i in range(n - 1):
+        #     prefixes[i + 1] *= prefixes[i] * nums[i]
+        #     suffixes[n - 2 - i] *= suffixes[n - 1 - i] * nums[n - 1 - i]
+        # ans[0], ans[n - 1] = suffixes[0], prefixes[n - 1]
+        # for i in range(1, n - 1):
+        #     ans[i] = prefixes[i] * suffixes[i]
+        # return ans
         
