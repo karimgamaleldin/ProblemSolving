c79b7753148f9b9fccca150ac5a2c6950f7d1e5b
Karim Gamaleldin <83180400+karimgamaleldin@users.noreply.github.com>
2024-07-30 18:03:41 +0300

    Time: 96 ms (33.65%), Space: 16.5 MB (40.21%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,28 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxs = [set() for _ in range(9)]
-        for i in range(9):
-            row = rows[i]
-            b = 0 
-            if i >= 3:
-                b = 3
-            if i >= 6:
-                b = 6
-            for j in range(9):
-                num = board[i][j]
-                if num == ".":
+        rows, cols, boxes = collections.defaultdict(set), collections.defaultdict(set), collections.defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                entry = board[r][c]
+                if entry == '.':
                     continue
-                col = cols[j]
-                box = boxs[b + j // 3]
-                if num in row or num in col or num in box:
-                    # print(num in row)
-                    # print(num in col)
-                    # print(num in box)
-                    # print(num, i, j)
+                if entry in rows[r] or entry in cols[c] or entry in boxes[(r // 3, c // 3)]:
                     return False
-                row.add(num)
-                col.add(num)
-                box.add(num)
+                rows[r].add(entry)
+                cols[c].add(entry)
+                boxes[(r // 3, c // 3)].add(entry)
+        return True
         return True
